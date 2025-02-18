e9dfefb15d9e64e7f39d46b435457dca537bab3c
Davide Mininni <davide.mininni@finconsgroup.com>
2025-02-18 18:11:48 +0100

    feat: add stories

---
+++ src/showcase/stories/angular/paginator/compact-paginator/compact-paginator.stories.ts
@@ -0,0 +1,50 @@
+import { SbbCompactPaginator } from '@sbb-esta/lyne-angular/paginator/compact-paginator';
+import { Args, Meta } from '@storybook/angular';
+import { ArgTypes, InputType, StoryContext } from '@storybook/types';
+
+import { spreadArgs } from '../../../../tools/spread-args';
+
+const pagerPosition: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['start', 'end'],
+};
+
+const size: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['m', 's'],
+};
+
+const argTypes: ArgTypes = {
+  'pager-position': pagerPosition,
+  size,
+};
+
+const args: Args = {
+  length: 100,
+  'page-size': 10,
+  'page-index': 0,
+  'pager-position': pagerPosition.options![0],
+  size: size.options![0],
+};
+
+const meta: Meta = {
+  title: 'elements/sbb-paginator/sbb-compact-paginator',
+  component: SbbCompactPaginator,
+  parameters: {
+    backgroundColor: (context: StoryContext) =>
+      context.args['negative'] ? 'var(--sbb-color-black)' : 'var(--sbb-color-white)',
+  },
+  argTypes,
+  args,
+  render: (args) => ({
+    props: { ...args },
+    template: `<sbb-compact-paginator aria-label="Select page" ${spreadArgs(args)}></sbb-compact-paginator>`,
+  }),
+};
+export default meta;
+
+export const Default = {};
---
+++ src/showcase/stories/angular/paginator/paginator/paginator.stories.ts
@@ -0,0 +1,69 @@
+import { SbbPaginator } from '@sbb-esta/lyne-angular/paginator/paginator';
+import { Args, Meta } from '@storybook/angular';
+import { ArgTypes, InputType, StoryContext } from '@storybook/types';
+
+import { spreadArgs } from '../../../../tools/spread-args';
+
+const pageSizeOptionsValues = [[], [10, 20, 50, 100], [10, 50, 100, 500]];
+const pageSizeOptions: InputType = {
+  options: Object.keys(pageSizeOptionsValues),
+  mapping: pageSizeOptionsValues,
+  control: {
+    type: 'select',
+    labels: {
+      0: 'none',
+      1: '10, 20, 50, 100',
+      2: '10, 50, 100, 500',
+    },
+  },
+};
+
+const pagerPosition: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['start', 'end'],
+};
+
+const size: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['m', 's'],
+};
+
+const argTypes: ArgTypes = {
+  pageSizeOptions,
+  'page-size-options': { control: false, table: { disable: true } },
+  'pager-position': pagerPosition,
+  size,
+};
+
+const args: Args = {
+  length: 100,
+  'page-size': 10,
+  'page-index': 0,
+  pageSizeOptions: pageSizeOptions.options![0],
+  'pager-position': pagerPosition.options![0],
+  size: size.options![0],
+};
+
+const meta: Meta = {
+  title: 'elements/sbb-paginator/sbb-paginator',
+  component: SbbPaginator,
+  parameters: {
+    backgroundColor: (context: StoryContext) =>
+      context.args['negative'] ? 'var(--sbb-color-black)' : 'var(--sbb-color-white)',
+  },
+  argTypes,
+  args,
+  render: ({ pageSizeOptions, ...args }) => ({
+    props: { pageSizeOptions, ...args },
+    template: `
+     <sbb-paginator aria-label="Select page" [page-size-options]="[${pageSizeOptions}]" ${spreadArgs(args)}></sbb-paginator>
+    `,
+  }),
+};
+export default meta;
+
+export const Default = {};
