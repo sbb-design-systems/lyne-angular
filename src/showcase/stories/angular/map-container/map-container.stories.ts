a3fd3d01c0cbd4009db6a72df17dc1ad148893b7
Davide Mininni <davide.mininni@finconsgroup.com>
2025-02-18 11:40:37 +0100

    feat: add stories

---
+++ src/showcase/stories/angular/map-container/map-container.stories.ts
@@ -0,0 +1,72 @@
+import { SbbFormField } from '@sbb-esta/lyne-angular/form-field/form-field';
+import { SbbFormFieldClear } from '@sbb-esta/lyne-angular/form-field/form-field-clear';
+import { SbbHeader } from '@sbb-esta/lyne-angular/header/header';
+import { SbbHeaderButton } from '@sbb-esta/lyne-angular/header/header-button';
+import { SbbIcon } from '@sbb-esta/lyne-angular/icon';
+import { SbbLogo } from '@sbb-esta/lyne-angular/logo';
+import { SbbMapContainer } from '@sbb-esta/lyne-angular/map-container';
+import { SbbTitle } from '@sbb-esta/lyne-angular/title';
+import { Meta, moduleMetadata } from '@storybook/angular';
+
+import { spreadArgs } from '../../../tools/spread-args';
+
+const meta: Meta = {
+  decorators: [
+    moduleMetadata({
+      imports: [
+        SbbFormField,
+        SbbIcon,
+        SbbTitle,
+        SbbHeader,
+        SbbLogo,
+        SbbHeaderButton,
+        SbbFormFieldClear,
+      ],
+    }),
+  ],
+  title: 'elements/sbb-map-container',
+  component: SbbMapContainer,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  render: (args) => ({
+    props: { ...args },
+    template: `
+      <sbb-header expanded>
+        <sbb-header-button icon-name="hamburger-menu-small" expand-from="small">
+          Menu
+        </sbb-header-button>
+        <div class="sbb-header-spacer"></div>
+        <sbb-logo protective-room="none" class="sbb-header-logo"></sbb-logo>
+      </sbb-header>
+      <sbb-map-container ${spreadArgs(args)}>
+        <div style="padding: var(--sbb-spacing-fixed-4x)">
+          <sbb-form-field style="width: 100%">
+            <sbb-icon slot="prefix" name="magnifying-glass-small"></sbb-icon>
+            <input minlength="2" name="keyword" autocomplete="off" placeholder="Search" />
+            <sbb-form-field-clear></sbb-form-field-clear>
+          </sbb-form-field>
+          <sbb-title level="4">Operations & Disruptions</sbb-title>
+          ${[...Array(10).keys()]
+            .map(
+              (value) => `
+                  <div style="background-color: var(--sbb-color-milk); height: 116px; display: flex; align-items: center; justify-content: center; border-radius: var(--sbb-border-radius-4x); margin-block-end: var(--sbb-spacing-fixed-4x);">
+                    <p>Situation ${value}</p>
+                  </div>
+                `,
+            )
+            .join('')}
+        </div>
+
+        <div slot="map" style="height: 100%">
+          <div style="background-color: grey; height: 100%; display: flex; align-items: center; justify-content: center;">
+            map
+          </div>
+        </div>
+      </sbb-map-container>
+    `,
+  }),
+};
+export default meta;
+
+export const Default = {};
---
+++ src/showcase/stories/angular/message/message.stories.ts
@@ -0,0 +1,51 @@
+import { SbbSecondaryButton } from '@sbb-esta/lyne-angular/button/secondary-button';
+import { SbbImage } from '@sbb-esta/lyne-angular/image';
+import { SbbMessage } from '@sbb-esta/lyne-angular/message';
+import { Args, Meta, moduleMetadata } from '@storybook/angular';
+import { ArgTypes, InputType } from '@storybook/types';
+
+import { spreadArgs } from '../../../tools/spread-args';
+
+const titleLevel: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: [1, 2, 3, 4, 5, 6],
+};
+
+const argTypes: ArgTypes = {
+  'title-level': titleLevel,
+};
+
+const args: Args = {
+  'title-content': 'Unfortunately, an error has occurred.',
+  'title-level': 3,
+};
+
+const meta: Meta = {
+  decorators: [
+    moduleMetadata({
+      imports: [SbbImage, SbbSecondaryButton],
+    }),
+  ],
+  title: 'elements/sbb-message',
+  component: SbbMessage,
+  argTypes,
+  args,
+  render: (args) => ({
+    props: { ...args },
+    template: `
+      <div style="max-width: 45rem; margin: auto">
+        <sbb-message ${spreadArgs(args)}>
+          <sbb-image slot="image" image-src='https://cdn.img.sbb.ch/content/dam/internet/lyne/Help-Teaser-Landscape.png'></sbb-image>
+          <p slot="subtitle">Please reload the page or try your search again later.</p>
+          <p slot="legend">Error code: 0001</p>
+          <sbb-secondary-button slot="action" icon-name="arrows-circle-small" size="m"></sbb-secondary-button>
+        </sbb-message>
+      </div>
+    `,
+  }),
+};
+export default meta;
+
+export const Default = {};
