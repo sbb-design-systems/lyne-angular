2e8fe87a793951218f1f0bfd92760a006a33a2fe
Davide Mininni <davide.mininni@finconsgroup.com>
2025-02-19 16:05:13 +0100

    feat: add stories

---
+++ src/showcase/stories/angular/screen-reader-only/screen-reader-only.stories.ts
@@ -0,0 +1,23 @@
+import { SbbScreenReaderOnly } from '@sbb-esta/lyne-angular/screen-reader-only';
+import { Meta } from '@storybook/angular';
+
+const meta: Meta = {
+  title: 'elements/sbb-screen-reader-only',
+  component: SbbScreenReaderOnly,
+  argTypes: {
+    content: { control: { type: 'text' } },
+  },
+  args: {
+    content: `I'm visually hidden, but read to screen reader.`,
+  },
+  render: (args) => ({
+    props: { ...args },
+    template: `
+      There is a visually hidden text here:
+      <sbb-screen-reader-only>${args['content']}</sbb-screen-reader-only>
+    `,
+  }),
+};
+export default meta;
+
+export const Default = {};
