68d94af14e4b7119805e6cfb8492ebc5c14dbb87
Davide Mininni <davide.mininni@finconsgroup.com>
2025-02-20 15:45:11 +0100

    feat: add stories

---
+++ src/showcase/stories/angular/tag/tag-group/tag-group.stories.ts
@@ -0,0 +1,66 @@
+import { SbbTag } from '@sbb-esta/lyne-angular/tag/tag';
+import { SbbTagGroup } from '@sbb-esta/lyne-angular/tag/tag-group';
+import { Args, Meta, moduleMetadata } from '@storybook/angular';
+import { ArgTypes, InputType } from '@storybook/types';
+
+import { spreadArgs } from '../../../../tools/spread-args';
+
+const label: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const value: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const size: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['s', 'm'],
+};
+
+const argTypes: ArgTypes = {
+  label,
+  value,
+  size,
+};
+
+const args: Args = {
+  label: 'Label',
+  multiple: true,
+  size: size.options![1],
+};
+
+const meta: Meta = {
+  decorators: [
+    moduleMetadata({
+      imports: [SbbTag],
+    }),
+  ],
+  title: 'elements/sbb-tag/sbb-tag-group',
+  component: SbbTagGroup,
+  argTypes,
+  args,
+  render: ({ label, ...args }) => ({
+    props: { label, ...args },
+    template: `
+      <sbb-tag-group ${spreadArgs(args)}>
+        ${new Array(8).fill(null).map(
+          (_e, i) => `
+          <sbb-tag [checked]=${i === 0} value=${label} amount="123" icon-name="pie-small">
+            ${label} ${i + 1}
+          </sbb-tag>
+        `,
+        )}
+      </sbb-tag-group>
+    `,
+  }),
+};
+export default meta;
+
+export const Default = {};
---
+++ src/showcase/stories/angular/tag/tag/tag.stories.ts
@@ -0,0 +1,42 @@
+import { SbbTag } from '@sbb-esta/lyne-angular/tag/tag';
+import { Args, Meta } from '@storybook/angular';
+import { ArgTypes, InputType } from '@storybook/types';
+
+import { spreadArgs } from '../../../../tools/spread-args';
+
+const label: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const size: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['s', 'm'],
+};
+
+const argTypes: ArgTypes = {
+  label,
+  size,
+};
+
+const args: Args = {
+  label: 'Label',
+  size: size.options![1],
+};
+
+const meta: Meta = {
+  title: 'elements/sbb-tag/sbb-tag',
+  component: SbbTag,
+  argTypes,
+  args,
+  render: ({ label, ...args }) => ({
+    props: { label, ...args },
+    template: `<sbb-tag ${spreadArgs(args)}>${label}</sbb-tag>`,
+  }),
+};
+export default meta;
+
+export const Default = {};
