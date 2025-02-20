ecb96be9a500f6fe0aaf28cda806d74965199d71
Davide Mininni <davide.mininni@finconsgroup.com>
2025-02-20 17:03:17 +0100

    feat: add stories

---
+++ src/showcase/stories/angular/teaser-product/teaser-product/teaser-product.stories.ts
@@ -0,0 +1,96 @@
+import { SbbButtonStatic } from '@sbb-esta/lyne-angular/button/button-static';
+import { SbbChipLabel } from '@sbb-esta/lyne-angular/chip-label';
+import { SbbTeaserProduct } from '@sbb-esta/lyne-angular/teaser-product/teaser-product';
+import { SbbTitle } from '@sbb-esta/lyne-angular/title';
+import { Args, Meta, moduleMetadata } from '@storybook/angular';
+import { ArgTypes, InputType, StoryContext } from '@storybook/types';
+
+import { spreadArgs } from '../../../../tools/spread-args';
+
+const imageAlignment: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['after', 'before'],
+};
+
+const withChip: InputType = {
+  control: {
+    type: 'boolean',
+  },
+};
+
+const withFooter: InputType = {
+  control: {
+    type: 'boolean',
+  },
+};
+
+const argTypes: ArgTypes = {
+  'image-alignment': imageAlignment,
+  withChip,
+  withFooter,
+};
+
+const args: Args = {
+  'image-alignment': imageAlignment.options![0],
+  withChip: true,
+  withFooter: true,
+  href: 'https://www.sbb.ch',
+  target: '_blank',
+};
+
+const meta: Meta = {
+  decorators: [
+    moduleMetadata({
+      imports: [SbbChipLabel, SbbTitle, SbbButtonStatic],
+    }),
+  ],
+  title: 'elements/sbb-teaser/sbb-teaser-product',
+  component: SbbTeaserProduct,
+  parameters: {
+    backgroundColor: (context: StoryContext) =>
+      context.args['negative'] ? 'var(--sbb-color-black)' : 'var(--sbb-color-white)',
+  },
+  argTypes,
+  args,
+  render: ({ withChip, withFooter, ...args }) => ({
+    props: { withChip, withFooter, ...args },
+    template: `
+      <sbb-teaser-product ${spreadArgs(args)}>
+        <figure slot="image" class="sbb-figure">
+          <img src='https://cdn.img.sbb.ch/content/dam/internet/lyne/Hoehenrundweg-Gryden-Lenk.jpg' alt="" />
+          ${
+            withChip
+              ? `<sbb-chip-label class=${args['image-alignment'] === 'after' ? 'sbb-figure-overlap-start-end' : 'sbb-figure-overlap-start-start'}>
+                  AI generated
+                </sbb-chip-label>`
+              : ''
+          }
+        </figure>
+        <sbb-title level="3" class="sbb-teaser-product--spacing">
+          Benefit from up to 70% discount
+        </sbb-title>
+        <p class="sbb-teaser-product--spacing">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium felis sit amet felis
+          viverra lacinia. Donec et enim mi. Aliquam erat volutpat. Proin ut odio tellus. Donec tempor mi
+          vel dapibus lobortis. Sed at ex sit amet leo suscipit fermentum. Donec consequat hendrerit
+          tortor, ut laoreet velit congue in.
+        </p>
+        <sbb-button-static class="sbb-teaser-product--spacing">Label</sbb-button-static>
+        ${
+          withFooter
+            ? `<p slot="footnote" class="sbb-teaser-product--spacing">
+                Footnote Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium felis sit
+                amet felis viverra lacinia. Donec et enim mi. Aliquam erat volutpat. Proin ut odio tellus. Donec
+                tempor mi vel dapibus lobortis.
+              </p>`
+            : ''
+        }
+      </sbb-teaser-product>
+    `,
+  }),
+};
+export default meta;
+
+export const Default = {};
---
+++ src/showcase/stories/angular/teaser/teaser.stories.ts
@@ -0,0 +1,94 @@
+import { SbbChipLabel } from '@sbb-esta/lyne-angular/chip-label';
+import { SbbTeaser } from '@sbb-esta/lyne-angular/teaser';
+import { Args, Meta, moduleMetadata } from '@storybook/angular';
+import { ArgTypes, InputType } from '@storybook/types';
+
+import { spreadArgs } from '../../../tools/spread-args';
+
+const titleContent: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const chipContent: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const alignment: InputType = {
+  control: {
+    type: 'select',
+  },
+  options: ['after-centered', 'after', 'below'],
+};
+
+const hrefs: string[] = [
+  'https://www.sbb.ch',
+  'https://github.com/sbb-design-systems/lyne-components',
+];
+const href: InputType = {
+  options: Object.keys(hrefs),
+  mapping: hrefs,
+  control: {
+    type: 'select',
+    labels: {
+      0: 'sbb.ch',
+      1: 'GitHub Lyne Components',
+    },
+  },
+};
+
+const description: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const argTypes: ArgTypes = {
+  'title-content': titleContent,
+  'chip-content': chipContent,
+  alignment,
+  href,
+  description,
+};
+
+const args: Args = {
+  'title-content': 'This is a title',
+  'chip-content': undefined,
+  alignment: alignment.options![0],
+  href: href.options![1],
+  description: 'This is a paragraph',
+  'accessibility-label':
+    'The text which gets exposed to screen reader users. The text should reflect all the information which gets passed into the components slots and which is visible in the Teaser, either through text or iconography',
+};
+
+const meta: Meta = {
+  decorators: [
+    moduleMetadata({
+      imports: [SbbChipLabel],
+    }),
+  ],
+  title: 'elements/sbb-teaser/sbb-teaser',
+  component: SbbTeaser,
+  argTypes,
+  args,
+  render: ({ description, ...args }) => ({
+    props: { description, ...args },
+    template: `
+      <div style="max-width: 760px;">
+        <sbb-teaser ${spreadArgs(args)}>
+          <figure slot="image" class="sbb-figure">
+            <img src="https://cdn.img.sbb.ch/content/dam/internet/lyne/Billetkontrolle.jpg" alt="400x300" />
+            <sbb-chip-label class="sbb-figure-overlap-start-start">AI Generated</sbb-chip-label>
+          </figure>
+          ${description}
+        </sbb-teaser>
+      </div>
+    `,
+  }),
+};
+export default meta;
+
+export const Default = {};
