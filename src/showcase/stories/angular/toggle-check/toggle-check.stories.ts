a26ceda8e284dbc30fa86ecad7b00782e86350db
Davide Mininni <davide.mininni@finconsgroup.com>
2025-02-21 11:28:10 +0100

    feat: add stories

---
+++ src/showcase/stories/angular/toggle-check/toggle-check.stories.ts
@@ -0,0 +1,56 @@
+import { SbbToggleCheck } from '@sbb-esta/lyne-angular/toggle-check';
+import { withActions } from '@storybook/addon-actions/decorator';
+import { Args, Meta } from '@storybook/angular';
+import { ArgTypes, InputType } from '@storybook/types';
+
+import { spreadArgs } from '../../../tools/spread-args';
+
+const size: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['xs', 's', 'm'],
+};
+
+const label: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const labelPosition: InputType = {
+  control: {
+    type: 'inline-radio',
+  },
+  options: ['before', 'after'],
+};
+
+const argTypes: ArgTypes = {
+  size,
+  label,
+  'label-position': labelPosition,
+};
+
+const args: Args = {
+  size: size.options![1],
+  label: 'Label',
+  'label-position': labelPosition.options![1],
+};
+
+const meta: Meta = {
+  decorators: [withActions],
+  title: 'elements/sbb-toggle/sbb-toggle-check',
+  component: SbbToggleCheck,
+  parameters: {
+    actions: { handles: ['click'] },
+  },
+  argTypes,
+  args,
+  render: ({ label, ...args }) => ({
+    props: { label, ...args },
+    template: `<sbb-toggle-check ${spreadArgs(args)}>${label}</sbb-toggle-check>`,
+  }),
+};
+export default meta;
+
+export const Default = {};
---
+++ src/showcase/stories/angular/toggle/toggle-option/toggle-option.stories.ts
@@ -0,0 +1,42 @@
+import { SbbToggleOption } from '@sbb-esta/lyne-angular/toggle/toggle-option';
+import { Args, Meta } from '@storybook/angular';
+import { ArgTypes, InputType } from '@storybook/types';
+
+import { spreadArgs } from '../../../../tools/spread-args';
+
+const label: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const iconName: InputType = {
+  control: {
+    type: 'select',
+  },
+  options: [null, 'arrow-right-small', 'app-icon-small', 'train-small', 'swisspass-small'],
+};
+
+const argTypes: ArgTypes = {
+  label,
+  'icon-name': iconName,
+};
+
+const args: Args = {
+  label: 'Option',
+  'icon-name': iconName.options![0],
+};
+
+const meta: Meta = {
+  title: 'elements/sbb-toggle/sbb-toggle-option',
+  component: SbbToggleOption,
+  argTypes,
+  args,
+  render: ({ label, 'icon-name': iconName, ...args }) => ({
+    props: { label, 'icon-name': iconName, ...args },
+    template: `<sbb-toggle-option ${iconName ? `icon-name="${iconName}"` : ''} ${spreadArgs(args)}>${label}</sbb-toggle-option>`,
+  }),
+};
+export default meta;
+
+export const Default = {};
