9c5cbc2a8357e602d2be2ce6dd52143668b4f142
Lukas Spirig <lukas.spirig@sbb.ch>
2025-03-17 12:55:57 +0100

    feat(sbb-date-input): add Angular Forms validation

--- src/angular/date-input/date-input.ts
+++ src/angular/date-input/date-input.ts
@@ -1,5 +1,13 @@
 import { Directive, ElementRef, Input, NgZone, Output, inject, forwardRef } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { booleanAttribute, SbbControlValueAccessorMixin } from '@sbb-esta/lyne-angular/core';
 import { readConfig } from '@sbb-esta/lyne-elements/core/config.js';
 import { defaultDateAdapter } from '@sbb-esta/lyne-elements/core/datetime.js';
@@ -11,15 +19,26 @@ import '@sbb-esta/lyne-elements/date-input.js';
 @Directive({
   selector: 'sbb-date-input',
   exportAs: 'sbbDateInput',
-  host: { '(change)': 'this.onChangeFn(this.valueAsDate)', '(blur)': 'this.onTouchedFn()' },
+  host: {
+    '(change)': 'this.onChangeFn(this.valueAsDate)',
+    '(blur)': 'this.onTouchedFn()',
+    '(input)': 'this.validatorOnChange()',
+    '(invalid)': 'this.validatorOnChange()',
+  },
   providers: [
     { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => SbbDateInput), multi: true },
+    { provide: NG_VALIDATORS, useExisting: forwardRef(() => SbbDateInput), multi: true },
   ],
 })
-export class SbbDateInput<T = Date> extends SbbControlValueAccessorMixin(class {}) {
+export class SbbDateInput<T = Date>
+  extends SbbControlValueAccessorMixin(class {})
+  implements Validator
+{
   #element: ElementRef<SbbDateInputElement<T>> = inject(ElementRef<SbbDateInputElement<T>>);
   #ngZone: NgZone = inject(NgZone);
   #dateAdapter = readConfig().datetime?.dateAdapter ?? defaultDateAdapter;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  protected validatorOnChange = () => {};
 
   @Input()
   public set value(value: string) {
@@ -141,6 +160,34 @@ export class SbbDateInput<T = Date> extends SbbControlValueAccessorMixin(class {
     return this.#element.nativeElement.willValidate;
   }
 
+  /** The form control validator for whether the input parses. */
+  private _parseValidator: ValidatorFn = (): ValidationErrors | null =>
+    this.validity.badInput ? { sbbDateParse: { text: this.#element.nativeElement.value } } : null;
+
+  /** The form control validator for the min date. */
+  private _minValidator: ValidatorFn = (): ValidationErrors | null =>
+    this.validity.rangeUnderflow
+      ? { sbbDateMin: { min: this.min, actual: this.valueAsDate } }
+      : null;
+
+  /** The form control validator for the max date. */
+  private _maxValidator: ValidatorFn = (): ValidationErrors | null =>
+    this.validity.rangeOverflow
+      ? { sbbDateMax: { max: this.max, actual: this.valueAsDate } }
+      : null;
+
+  /** The form control validator for the date filter. */
+  private _filterValidator: ValidatorFn = (): ValidationErrors | null =>
+    this.validity.sbbDateFilter ? { sbbDateFilter: true } : null;
+
+  /** The combined form control validator for this input. */
+  private _validator: ValidatorFn | null = Validators.compose([
+    this._parseValidator,
+    this._minValidator,
+    this._maxValidator,
+    this._filterValidator,
+  ]);
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   override writeValue(value: any): void {
     if (this.#dateAdapter.isDateInstance(value) && this.#dateAdapter.isValid(value)) {
@@ -151,6 +198,14 @@ export class SbbDateInput<T = Date> extends SbbControlValueAccessorMixin(class {
         this.value = value;
       }
     }
+  }
+
+  registerOnValidatorChange?(fn: () => void): void {
+    this.validatorOnChange = fn;
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    return this._validator?.(control) ?? null;
   }
 
   public focus(options: FocusOptions): void {
