9ec4b8a9cb8bfac6aea8231daf2d9f462c4851e9
Jeremias Peier <jeremias.peier@sbb.ch>
2025-03-25 08:41:49 +0100

    fix: icon sidebar items

--- src/showcase/stories/angular/sidebar/icon-sidebar-button/icon-sidebar-button.stories.ts
+++ src/showcase/stories/angular/sidebar/icon-sidebar-button/icon-sidebar-button.stories.ts
@@ -1,14 +1,53 @@
 import { SbbIconSidebarButton } from '@sbb-esta/lyne-angular/sidebar/icon-sidebar-button';
 import { withActions } from '@storybook/addon-actions/decorator';
-import { Args, argsToTemplate, Meta, moduleMetadata } from '@storybook/angular';
+import {
+  Args,
+  argsToTemplate,
+  ArgTypes,
+  componentWrapperDecorator,
+  Meta,
+} from '@storybook/angular';
+import { InputType } from '@storybook/types';
+
+const ariaLabel: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const iconName: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const currentPage: InputType = {
+  control: {
+    type: 'boolean',
+  },
+};
+
+const argTypes: ArgTypes = {
+  'icon-name': iconName,
+  'aria-label': ariaLabel,
+  currentPage,
+};
+
+const args: Args = {
+  'icon-name': 'glass-cocktail-small',
+  'aria-label': 'Go to the party',
+  currentPage: false,
+};
 
 const meta: Meta = {
   decorators: [
     withActions,
-    moduleMetadata({
-      // add slotted components or remove
-      imports: [],
-    }),
+    componentWrapperDecorator(
+      (story) => `<div style="max-width: var(--sbb-size-element-m)">
+        <!-- The max-width is only set for storybook. Don't copy it. -->
+        ${story}
+      </div>`,
+    ),
   ],
   title: 'elements/sbb-sidebar/sbb-icon-sidebar-button',
   component: SbbIconSidebarButton,
@@ -16,9 +55,15 @@ const meta: Meta = {
     // add events or remove the 'action' object
     actions: { handles: ['click'] },
   },
-  render: (args: Args) => ({
-    props: { ...args },
-    template: `<sbb-icon-sidebar-button ${argsToTemplate(args)}></sbb-icon-sidebar-button>`,
+  argTypes,
+  args,
+  render: ({ currentPage, ...args }) => ({
+    props: { currentPage, ...args },
+    template: `<sbb-icon-sidebar-button
+                 ${argsToTemplate(args)}
+                 [class.sbb-active]=${currentPage}
+                 aria-current=${currentPage ? 'page' : ''}
+               ></sbb-icon-sidebar-button>`,
   }),
 };
 export default meta;
--- src/showcase/stories/angular/sidebar/icon-sidebar-link/icon-sidebar-link.stories.ts
+++ src/showcase/stories/angular/sidebar/icon-sidebar-link/icon-sidebar-link.stories.ts
@@ -1,14 +1,61 @@
 import { SbbIconSidebarLink } from '@sbb-esta/lyne-angular/sidebar/icon-sidebar-link';
 import { withActions } from '@storybook/addon-actions/decorator';
-import { Args, argsToTemplate, Meta, moduleMetadata } from '@storybook/angular';
+import {
+  Args,
+  argsToTemplate,
+  ArgTypes,
+  componentWrapperDecorator,
+  Meta,
+} from '@storybook/angular';
+import { InputType } from '@storybook/types';
+
+const accessibilityLabel: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const iconName: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const href: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const currentPage: InputType = {
+  control: {
+    type: 'boolean',
+  },
+};
+
+const argTypes: ArgTypes = {
+  'icon-name': iconName,
+  'accessibility-label': accessibilityLabel,
+  href,
+  currentPage,
+};
+
+const args: Args = {
+  'icon-name': 'glass-cocktail-small',
+  'accessibility-label': 'Go to the party',
+  href: '#',
+  currentPage: false,
+};
 
 const meta: Meta = {
   decorators: [
     withActions,
-    moduleMetadata({
-      // add slotted components or remove
-      imports: [],
-    }),
+    componentWrapperDecorator(
+      (story) => `<div style="max-width: var(--sbb-size-element-m)">
+        <!-- The max-width is only set for storybook. Don't copy it. -->
+        ${story}
+      </div>`,
+    ),
   ],
   title: 'elements/sbb-sidebar/sbb-icon-sidebar-link',
   component: SbbIconSidebarLink,
@@ -16,9 +63,15 @@ const meta: Meta = {
     // add events or remove the 'action' object
     actions: { handles: ['click'] },
   },
-  render: (args: Args) => ({
-    props: { ...args },
-    template: `<sbb-icon-sidebar-link ${argsToTemplate(args)}></sbb-icon-sidebar-link>`,
+  argTypes,
+  args,
+  render: ({ currentPage, ...args }) => ({
+    props: { currentPage, ...args },
+    template: `<sbb-icon-sidebar-link
+                ${argsToTemplate(args)}
+                 [class.sbb-active]=${currentPage}
+                 accessibility-current=${currentPage ? 'page' : ''}
+               ></sbb-icon-sidebar-link>`,
   }),
 };
 export default meta;
