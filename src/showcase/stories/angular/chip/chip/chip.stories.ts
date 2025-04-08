c3b187e32964dc32abf475a3c8d6c45642ebe7ff
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2025-04-08 11:24:14 +0200

    fix(sbb-chip-group): feedbacks

--- src/showcase/stories/angular/chip/chip/chip.stories.ts
+++ src/showcase/stories/angular/chip/chip/chip.stories.ts
@@ -1,9 +1,7 @@
 import { SbbChip } from '@sbb-esta/lyne-angular/chip/chip';
-import { withActions } from '@storybook/addon-actions/decorator';
 import { Args, argsToTemplate, Meta } from '@storybook/angular';
 
 const meta: Meta = {
-  decorators: [withActions],
   title: 'elements/sbb-chip/sbb-chip',
   component: SbbChip,
   render: (args: Args) => ({
