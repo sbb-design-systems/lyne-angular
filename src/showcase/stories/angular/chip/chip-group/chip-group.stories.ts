d69dd983ef21af917463627f7c3531db33af1aa2
Davide Mininni <davide.mininni@finconsgroup.com>
2025-04-09 11:17:04 +0200

    chore: stories improvements

--- src/showcase/stories/angular/chip/chip-group/chip-group.stories.ts
+++ src/showcase/stories/angular/chip/chip-group/chip-group.stories.ts
@@ -1,10 +1,19 @@
 import { SbbChip } from '@sbb-esta/lyne-angular/chip/chip';
 import { SbbChipGroup } from '@sbb-esta/lyne-angular/chip/chip-group';
 import { SbbFormField } from '@sbb-esta/lyne-angular/form-field/form-field';
-import { Args, Meta, moduleMetadata } from '@storybook/angular';
-import { ArgTypes, InputType } from '@storybook/types';
+import { Args, argsToTemplate, Meta, moduleMetadata } from '@storybook/angular';
+import { ArgTypes, InputType, StoryContext } from '@storybook/types';
 
 const readonly: InputType = {
+  control: {
+    type: 'boolean',
+  },
+  table: {
+    category: 'Input field',
+  },
+};
+
+const disabledInput: InputType = {
   control: {
     type: 'boolean',
   },
@@ -41,16 +50,43 @@ const floatingLabel: InputType = {
   },
 };
 
+const separatorKeys: InputType = {
+  control: {
+    type: 'text',
+  },
+};
+
+const value: InputType = {
+  control: false,
+  table: { disable: true },
+};
+
+const name: InputType = {
+  control: false,
+  table: { disable: true },
+};
+
+const required: InputType = {
+  control: false,
+  table: { disable: true },
+};
+
 const argTypes: ArgTypes = {
   readonly,
+  disabledInput,
   size,
   hiddenLabel,
   floatingLabel,
+  separatorKeys,
+  value,
+  name,
+  required,
 };
 
 const args: Args = {
-  disabled: false,
   readonly: false,
+  disabledInput: false,
+  disabled: false,
   negative: false,
   size: 'm',
   hiddenLabel: false,
@@ -65,23 +101,35 @@ const meta: Meta = {
   ],
   title: 'elements/sbb-chip/sbb-chip-group',
   component: SbbChipGroup,
+  parameters: {
+    backgroundColor: (context: StoryContext) =>
+      context.args['negative'] ? 'var(--sbb-color-black)' : 'var(--sbb-color-white)',
+  },
   argTypes,
   args,
-  render: ({ negative, size, hiddenLabel, floatingLabel, disabled, readonly }: Args) => ({
-    props: { negative, size, hiddenLabel, floatingLabel, disabled, readonly },
+  render: ({
+    negative,
+    size,
+    hiddenLabel,
+    floatingLabel,
+    readonly,
+    disabledInput,
+    ...args
+  }: Args) => ({
+    props: { negative, size, hiddenLabel, floatingLabel, readonly, disabledInput, ...args },
     template: `
       <sbb-form-field
         [negative]="negative"
         [size]="size"
-        [hidden-label]="hiddenLabel"
-        [floating-label]="floatingLabel"
+        [hiddenLabel]="hiddenLabel"
+        [floatingLabel]="floatingLabel"
       >
         <label>Label</label>
-        <sbb-chip-group>
+        <sbb-chip-group ${argsToTemplate(args)}>
           <sbb-chip value="chip 1"></sbb-chip>
           <sbb-chip value="chip 2"></sbb-chip>
           <sbb-chip value="chip 3"></sbb-chip>
-          <input placeholder="Placeholder" [disabled]="disabled" [readonly]="readonly" />
+          <input placeholder="Placeholder" [disabled]="disabledInput" [readonly]="readonly" />
         </sbb-chip-group>
       </sbb-form-field>
     `,
