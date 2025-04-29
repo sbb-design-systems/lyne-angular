ee86b11de0ef6ac260dacd41b3849fb7c7eaf92c
Jeremias Peier <jeremias.peier@sbb.ch>
2025-04-29 13:36:25 +0200

    test: add tests to checkbox family

--- src/angular-experimental/timetable-row/timetable-row.spec.ts
+++ src/angular-experimental/timetable-row/timetable-row.spec.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
+import type { ComponentFixture } from '@angular/core/testing';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SbbTimetableRow } from '@sbb-esta/lyne-angular-experimental/timetable-row';
+import type { SbbTimetableRowElement } from '@sbb-esta/lyne-elements-experimental/timetable-row.js';
 
 describe('timetable-row', () => {
-  beforeEach(waitForAsync(() => {
+  let fixture: ComponentFixture<TestComponent>, lyneElement: SbbTimetableRowElement;
+
+  beforeEach(waitForAsync(async () => {
     TestBed.configureTestingModule({
       imports: [NoopAnimationsModule],
     });
+
+    await customElements.whenDefined('sbb-timetable-row');
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+
+    lyneElement = fixture.debugElement.nativeElement.querySelector('sbb-timetable-row')!;
   }));
 
-  it('should load', async () => {
-    await customElements.whenDefined('sbb-timetable-row');
-    const fixture = TestBed.createComponent(TimetableRowTest);
-    fixture.detectChanges();
+  it('should be defined', async () => {
+    expect(lyneElement.matches(':defined')).toBeTrue();
   });
 });
 
@@ -21,4 +29,4 @@ describe('timetable-row', () => {
   template: `<sbb-timetable-row></sbb-timetable-row>`,
   imports: [SbbTimetableRow],
 })
-class TimetableRowTest {}
+class TestComponent {}
---
+++ src/angular/checkbox/checkbox-panel/checkbox-panel.spec.ts
@@ -0,0 +1,118 @@
+import type { ElementRef } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import type { SbbCheckboxPanelElement } from '@sbb-esta/lyne-elements/checkbox/checkbox-panel.js';
+
+import { SbbCheckboxPanel } from './checkbox-panel';
+
+describe('checkbox-panel', () => {
+  let fixture: ComponentFixture<TestComponent>, lyneElement: SbbCheckboxPanelElement;
+
+  beforeEach(waitForAsync(async () => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule],
+    });
+
+    await customElements.whenDefined('sbb-checkbox-panel');
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+
+    lyneElement = fixture.debugElement.nativeElement.querySelector('sbb-checkbox-panel')!;
+  }));
+
+  it('should not be checked', async () => {
+    expect(lyneElement.checked).toBeFalse();
+  });
+
+  it('should be checked', async () => {
+    fixture.componentInstance.control.setValue(true);
+
+    expect(lyneElement.checked).toBeTrue();
+  });
+
+  it('should uncheck', async () => {
+    fixture.componentInstance.control.setValue(true);
+    expect(lyneElement.checked).toBeTrue();
+
+    fixture.componentInstance.control.setValue(false);
+    expect(lyneElement.checked).toBeFalse();
+  });
+
+  it('should check by click and update ng-touched and ng-pristine', async () => {
+    expect(lyneElement).toHaveClass('ng-untouched');
+    expect(lyneElement).toHaveClass('ng-pristine');
+
+    lyneElement.focus();
+    lyneElement.click();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(lyneElement.checked).toBeTrue();
+    expect(fixture.componentInstance.control.value).toBeTrue();
+
+    fixture.componentInstance.button()!.nativeElement!.focus();
+    lyneElement.dispatchEvent(new FocusEvent('blur'));
+
+    // We need to wait two cycles until touched is set on host
+    fixture.detectChanges();
+    await fixture.whenStable();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(lyneElement).toHaveClass('ng-dirty');
+    expect(lyneElement).toHaveClass('ng-touched');
+  });
+
+  it('should be unchecked by click', async () => {
+    fixture.componentInstance.control.setValue(true);
+
+    lyneElement.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(lyneElement.checked).toBeFalse();
+    expect(fixture.componentInstance.control.value).toBeFalse();
+  });
+
+  it('should handle disabled', async () => {
+    fixture.componentInstance.control.disable();
+    expect(lyneElement.disabled).toBeTrue();
+
+    fixture.componentInstance.control.enable();
+    expect(lyneElement.disabled).toBeFalse();
+  });
+
+  it('should handle validation', async () => {
+    fixture.componentInstance.control.addValidators(Validators.requiredTrue);
+    fixture.componentInstance.control.setValue(false);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(lyneElement).toHaveClass('ng-invalid');
+
+    lyneElement.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(lyneElement).toHaveClass('ng-valid');
+  });
+});
+
+@Component({
+  template: `<form>
+    <sbb-checkbox-panel [formControl]="control">Checkbox</sbb-checkbox-panel>
+    <button #button (click)="buttonClicked($event)">Blur</button>
+  </form>`,
+  imports: [SbbCheckboxPanel, ReactiveFormsModule],
+})
+class TestComponent {
+  control = new FormControl(false);
+  button = viewChild<ElementRef<HTMLButtonElement>>('button');
+  buttonClicked(event: Event) {
+    // Prevent test page reload
+    event.preventDefault();
+  }
+}
