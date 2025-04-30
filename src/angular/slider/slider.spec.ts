e379912752f2d877c099bac7ee936e6703ddfd18
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2025-04-30 16:50:09 +0200

    test(sbb-slider): add spec tests

--- src/angular/slider/slider.spec.ts
+++ src/angular/slider/slider.spec.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { type ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { SbbIcon } from '../icon';
 
 import { SbbSlider } from './slider';
 
@@ -14,11 +17,21 @@ describe('sbb-slider', () => {
 
   it('should create', async () => {
     expect(component).toBeDefined();
+    expect(component.slider.value).toBe('10');
+
+    component.control.setValue('120');
+    expect(component.slider.value).toBe('100');
+
+    component.control.setValue('-20');
+    expect(component.slider.value).toBe('0');
   });
 });
 
 @Component({
-  template: `<sbb-slider></sbb-slider>`,
-  imports: [SbbSlider],
+  template: `<sbb-slider [formControl]="control" min="0" max="100" />`,
+  imports: [SbbSlider, SbbIcon, ReactiveFormsModule],
 })
-class TestComponent {}
+class TestComponent {
+  @ViewChild(SbbSlider, { static: true }) slider!: SbbSlider;
+  control = new FormControl('10');
+}
