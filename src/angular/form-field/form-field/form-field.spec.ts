cd67c602050d131aea96c936f89647691287f94b
Jeremias Peier <jeremias.peier@sbb.ch>
2025-04-30 15:55:40 +0200

    test: form field invalid (pending elements fix)

--- src/angular/form-field/form-field/form-field.spec.ts
+++ src/angular/form-field/form-field/form-field.spec.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { type ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { SbbFormField } from './form-field';
 
@@ -15,10 +16,29 @@ describe('sbb-form-field', () => {
   it('should create', async () => {
     expect(component).toBeDefined();
   });
+
+  it('should detect invalid state', async () => {
+    component.control.markAsTouched();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.control.invalid).toBe(true);
+    expect(component.control.touched).toBe(true);
+    expect(
+      (fixture.nativeElement as HTMLElement)
+        .querySelector('sbb-form-field')!
+        .hasAttribute('data-invalid'),
+    ).toBe(true);
+  });
 });
 
 @Component({
-  template: `<sbb-form-field></sbb-form-field>`,
-  imports: [SbbFormField],
+  template: `<sbb-form-field><input [formControl]="control" #input /></sbb-form-field>`,
+  imports: [SbbFormField, ReactiveFormsModule],
 })
-class TestComponent {}
+class TestComponent {
+  control = new FormControl('', Validators.required);
+  input = viewChild<ElementRef<HTMLInputElement>>(ElementRef<HTMLInputElement>);
+  formField = viewChild<ElementRef<SbbFormField>>(ElementRef<SbbFormField>);
+}
