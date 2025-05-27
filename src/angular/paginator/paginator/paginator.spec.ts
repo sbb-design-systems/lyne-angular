464b7b946f58f8cd163cc5b8e70ee8a2aacb7915
Jeremias Peier <jeremias.peier@sbb.ch>
2025-05-27 14:30:31 +0200

    test(paginator): add some more tests

--- src/angular/paginator/paginator/paginator.spec.ts
+++ src/angular/paginator/paginator/paginator.spec.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
 import { type ComponentFixture, TestBed } from '@angular/core/testing';
+import { take } from 'rxjs';
 
 import { SbbPaginator } from './paginator';
 
@@ -12,13 +13,72 @@ describe('sbb-paginator', () => {
     fixture.detectChanges();
   });
 
-  it('should create', async () => {
+  it('should create and emit initialized', async () => {
     expect(component).toBeDefined();
+
+    // We wait to ensure the paginator is initialized event is triggered when subscribing later.
+    await new Promise((resolve) => setTimeout(resolve, 10));
+    let called = false;
+
+    component
+      .paginator()
+      .initialized.pipe(take(1))
+      .subscribe(() => {
+        called = true;
+      });
+    await new Promise((resolve) => setTimeout(resolve, 0));
+
+    expect(called).toBeTrue();
+  });
+
+  it('should not emit page event during initialization', async () => {
+    const pageEventSpy = spyOn(component, 'page');
+
+    // We wait to wait a little bit to capture potential events.
+    await new Promise((resolve) => setTimeout(resolve, 10));
+
+    expect(pageEventSpy).not.toHaveBeenCalled();
+  });
+
+  it('should not emit page event when changing length', async () => {
+    const pageEventSpy = spyOn(component, 'page');
+
+    component.paginator().length = 100;
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(pageEventSpy).not.toHaveBeenCalled();
+  });
+
+  it('should emit page event when changing pageSize', async () => {
+    const pageEventSpy = spyOn(component, 'page');
+
+    component.paginator().pageSize = 1;
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(pageEventSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should emit page event when changing pageIndex', async () => {
+    const pageEventSpy = spyOn(component, 'page');
+
+    component.paginator().pageSize = 1;
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(pageEventSpy).toHaveBeenCalledTimes(1);
   });
 });
 
 @Component({
-  template: `<sbb-paginator></sbb-paginator>`,
+  template: `<sbb-paginator (page)="page($event)"></sbb-paginator>`,
   imports: [SbbPaginator],
 })
-class TestComponent {}
+class TestComponent {
+  paginator = viewChild.required(SbbPaginator);
+
+  page() {
+    // no-op
+  }
+}
