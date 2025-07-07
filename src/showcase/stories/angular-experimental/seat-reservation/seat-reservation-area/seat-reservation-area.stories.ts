df29abebd72d0c7b004d864a327993d5795016d4
Svetlana Komandirova <svetlana.komandirova@sbb.ch>
2025-07-07 14:04:10 +0200

    chore(seat-reservation): added stories for seat-reservation subcomponenten

--- src/showcase/stories/angular-experimental/seat-reservation/seat-reservation-area/seat-reservation-area.stories.ts
+++ src/showcase/stories/angular-experimental/seat-reservation/seat-reservation-area/seat-reservation-area.stories.ts
@@ -1,22 +1,16 @@
 import { SbbSeatReservationArea } from '@sbb-esta/lyne-angular-experimental/seat-reservation/seat-reservation-area';
 import type { Args, Meta } from '@storybook/angular';
-import { argsToTemplate, moduleMetadata } from '@storybook/angular';
-import { withActions } from 'storybook/actions/decorator';
+import { argsToTemplate } from '@storybook/angular';
+
+const defaultArgs: Args = {
+  style: '--sbb-reservation-area-width: 100;--sbb-reservation-area-height: 50;',
+  background: 'dark',
+  mounting: 'free',
+};
 
 const meta: Meta = {
-  decorators: [
-    withActions,
-    moduleMetadata({
-      // add slotted components or remove
-      imports: [],
-    }),
-  ],
   title: 'experimental/sbb-seat-reservation/sbb-seat-reservation-area',
   component: SbbSeatReservationArea,
-  parameters: {
-    // add events or remove the 'action' object
-    actions: { handles: ['click'] },
-  },
   render: (args: Args) => ({
     props: { ...args },
     template: `<sbb-seat-reservation-area ${argsToTemplate(args)}></sbb-seat-reservation-area>`,
@@ -24,4 +18,8 @@ const meta: Meta = {
 };
 export default meta;
 
-export const Default = {};
+export const Default = {
+  args: {
+    ...defaultArgs,
+  },
+};
