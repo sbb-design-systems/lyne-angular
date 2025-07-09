59c1d8d9c2b4738cd72dc33367e2550caf8d3522
sk-sbb <svetlana.komandirova@sbb.ch>
2025-07-09 09:46:06 +0200

    docs(sbb-seat-reservation): extended default storybook stories with data (#143)

--- src/showcase/stories/angular-experimental/seat-reservation/seat-reservation-navigation-services/seat-reservation-navigation-services.stories.ts
+++ src/showcase/stories/angular-experimental/seat-reservation/seat-reservation-navigation-services/seat-reservation-navigation-services.stories.ts
@@ -1,22 +1,14 @@
 import { SbbSeatReservationNavigationServices } from '@sbb-esta/lyne-angular-experimental/seat-reservation/seat-reservation-navigation-services';
 import type { Args, Meta } from '@storybook/angular';
-import { argsToTemplate, moduleMetadata } from '@storybook/angular';
-import { withActions } from 'storybook/actions/decorator';
+import { argsToTemplate } from '@storybook/angular';
+
+const defaultArgs: Args = {
+  propertyIds: ['BISTRO'],
+};
 
 const meta: Meta = {
-  decorators: [
-    withActions,
-    moduleMetadata({
-      // add slotted components or remove
-      imports: [],
-    }),
-  ],
   title: 'experimental/sbb-seat-reservation/sbb-seat-reservation-navigation-services',
   component: SbbSeatReservationNavigationServices,
-  parameters: {
-    // add events or remove the 'action' object
-    actions: { handles: ['click'] },
-  },
   render: (args: Args) => ({
     props: { ...args },
     template: `<sbb-seat-reservation-navigation-services ${argsToTemplate(args)}></sbb-seat-reservation-navigation-services>`,
@@ -24,4 +16,12 @@ const meta: Meta = {
 };
 export default meta;
 
-export const Default = {};
+export const Bistro = {
+  args: {
+    ...defaultArgs,
+  },
+};
+
+export const MultipleServiceIcons = {
+  args: { ...defaultArgs, propertyIds: ['BISTRO', 'SILENCE', 'WHEELCHAIR'] },
+};
