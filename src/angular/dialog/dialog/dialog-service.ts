e38c956964d184b2036872d054f121bdbf0ef1ff
Mario Castigliano <mario.castigliano@finconsgroup.com>
2025-07-17 10:25:08 +0200

    feat: dialog-service

---
+++ src/angular/dialog/dialog/dialog-service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable, Injector } from '@angular/core';
+import {
+  _SbbOverlayBaseService,
+  SBB_OVERLAY_DATA,
+  SbbOverlayRef,
+} from '@sbb-esta/lyne-angular/core/overlay';
+
+import type { SbbDialog } from './dialog';
+import { SbbDialogContainer } from './dialog-container';
+
+@Injectable({ providedIn: 'any' })
+export class SbbDialogService extends _SbbOverlayBaseService<SbbDialogContainer, SbbDialog> {
+  constructor() {
+    const injector = inject(Injector);
+    const parentDialog = inject(SbbDialogService, { optional: true, skipSelf: true });
+
+    super(injector, parentDialog, SbbDialogContainer, SbbOverlayRef, SBB_OVERLAY_DATA);
+  }
+}
