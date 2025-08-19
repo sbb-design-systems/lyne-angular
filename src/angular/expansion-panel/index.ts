8df4012856f23a3753454d7db808d67be9613554
Jeremias Peier <jeremias.peier@sbb.ch>
2025-08-19 17:19:08 +0200

    refactor: migrate expansion panel

---
+++ src/angular/expansion-panel/index.ts
@@ -0,0 +1,3 @@
+export * from './expansion-panel/expansion-panel';
+export * from './expansion-panel-content/expansion-panel-content';
+export * from './expansion-panel-header/expansion-panel-header';
--- src/showcase/stories/angular/accordion/accordion.stories.ts
+++ src/showcase/stories/angular/accordion/accordion.stories.ts
@@ -1,7 +1,9 @@
 import { SbbAccordion } from '@sbb-esta/lyne-angular/accordion';
-import { SbbExpansionPanel } from '@sbb-esta/lyne-angular/expansion-panel/expansion-panel';
-import { SbbExpansionPanelContent } from '@sbb-esta/lyne-angular/expansion-panel/expansion-panel-content';
-import { SbbExpansionPanelHeader } from '@sbb-esta/lyne-angular/expansion-panel/expansion-panel-header';
+import {
+  SbbExpansionPanel,
+  SbbExpansionPanelContent,
+  SbbExpansionPanelHeader,
+} from '@sbb-esta/lyne-angular/expansion-panel';
 import type { Args, Meta } from '@storybook/angular';
 import { argsToTemplate, moduleMetadata } from '@storybook/angular';
 import { withActions } from 'storybook/actions/decorator';
