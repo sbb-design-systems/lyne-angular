1fb2c5353442d795bbf520309fb1a09fc66c14f8
Jeremias Peier <jeremias.peier@sbb.ch>
2025-08-20 08:46:47 +0200

    refactor: migrate table

--- src/angular/autocomplete/autocomplete.spec.ts
+++ src/angular/autocomplete/autocomplete.spec.ts
@@ -1,9 +1,8 @@
 import { Component, viewChild, viewChildren } from '@angular/core';
 import { type ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-
-import { SbbFormField } from '../form-field/form-field';
-import { SbbOption } from '../option/option';
+import { SbbFormField } from '@sbb-esta/lyne-angular/form-field';
+import { SbbOption } from '@sbb-esta/lyne-angular/option/option';
 
 import { SbbAutocomplete } from './autocomplete';
 import { SbbAutocompleteTrigger } from './autocomplete-trigger';
