411cc2163e284d1edf1ca9f9569b8c6ed8b70320
Jeri Peier <jeremias.peier@sbb.ch>
2026-01-05 13:40:10 +0100

    fix(sbb-tabs): fix rendering time of tabs (#242)
    Previously tab rendering in lyne angular was broken (depending on
    rendering time). This fix simplifies the logic and fixes various cases.

--- src/angular/tabs/tab/tab-content.spec.ts
+++ src/angular/tabs/tab/tab-content.spec.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import type { ComponentFixture } from '@angular/core/testing';
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import type { SbbTab } from '@sbb-esta/lyne-angular/tabs';
 import type { SbbTabLabelElement } from '@sbb-esta/lyne-elements/tabs/tab-label.js';
 
 import { SbbTabsModule } from '../tabs.module';
@@ -31,21 +33,100 @@ describe(`sbb-tab-content`, () => {
     lazyTabLabelElement.click();
     fixture.detectChanges();
     await fixture.whenRenderingDone();
-    expect(lazyTabElement.textContent).toEqual('Lazy');
+    expect(lazyTabElement.textContent).toEqual('Lazy content');
+  });
+
+  it('should accept replaced lazy content', async () => {
+    const lazyTabElement = (fixture.nativeElement as HTMLElement).querySelector('#lazy')!;
+    const lazyTabLabelElement = (
+      fixture.nativeElement as HTMLElement
+    ).querySelector<SbbTabLabelElement>('#lazy-label')!;
+    lazyTabLabelElement.click();
+    fixture.detectChanges();
+    expect(lazyTabElement.textContent).toEqual('Lazy content');
+
+    fixture.componentInstance.replaceLazyContent.set(true);
+    fixture.detectChanges();
+
+    expect(lazyTabElement.textContent).toEqual('Replaced Lazy');
+  });
+
+  it('should accept null', async () => {
+    const lazyTabElement = (fixture.nativeElement as HTMLElement).querySelector('#lazy')!;
+    const lazyTabLabelElement = (
+      fixture.nativeElement as HTMLElement
+    ).querySelector<SbbTabLabelElement>('#lazy-label')!;
+    lazyTabLabelElement.click();
+    fixture.detectChanges();
+    expect(lazyTabElement.textContent).toEqual('Lazy content');
+
+    fixture.componentInstance.showContent.set(false);
+    fixture.detectChanges();
+
+    expect(lazyTabElement.textContent).toEqual('');
+  });
+
+  it('should avoid rendering on second activation', async () => {
+    const lazyTab = fixture.debugElement.query(By.css('#lazy'));
+
+    const lazyTabElement = (fixture.nativeElement as HTMLElement).querySelector('#lazy')!;
+    const lazyTabLabelElement = (
+      fixture.nativeElement as HTMLElement
+    ).querySelector<SbbTabLabelElement>('#lazy-label')!;
+    lazyTabLabelElement.click();
+    fixture.detectChanges();
+    expect(lazyTabElement.textContent).toEqual('Lazy content');
+    const contentPortal = (lazyTab.componentInstance as SbbTab).contentPortal;
+
+    const eagerTabLabel = (fixture.nativeElement as HTMLElement).querySelector<SbbTabLabelElement>(
+      '#eager-label',
+    )!;
+    eagerTabLabel.click();
+    fixture.detectChanges();
+
+    lazyTabLabelElement.click();
+    fixture.detectChanges();
+
+    expect((lazyTab.componentInstance as SbbTab).contentPortal).toBe(contentPortal);
+  });
+
+  it('should update template changed of lazy tabs', async () => {
+    const lazyTabElement = (fixture.nativeElement as HTMLElement).querySelector('#lazy')!;
+    const lazyTabLabelElement = (
+      fixture.nativeElement as HTMLElement
+    ).querySelector<SbbTabLabelElement>('#lazy-label')!;
+    lazyTabLabelElement.click();
+    fixture.detectChanges();
+    expect(lazyTabElement.textContent).toEqual('Lazy content');
+
+    fixture.componentInstance.content.set('Replaced Lazy');
+    fixture.detectChanges();
+
+    expect(lazyTabElement.textContent).toEqual('Replaced Lazy content');
   });
 });
 
 @Component({
   template: `
     <sbb-tab-group>
-      <sbb-tab-label active>Eager loading</sbb-tab-label>
+      <sbb-tab-label active id="eager-label">Eager loading</sbb-tab-label>
       <sbb-tab id="eager">Eager content</sbb-tab>
       <sbb-tab-label id="lazy-label">Lazy loading</sbb-tab-label>
       <sbb-tab id="lazy">
-        <ng-template sbbTabContent>Lazy</ng-template>
+        @if (showContent()) {
+          @if (replaceLazyContent()) {
+            <ng-template sbbTabContent>Replaced Lazy</ng-template>
+          } @else {
+            <ng-template sbbTabContent>{{ content() }} content</ng-template>
+          }
+        }
       </sbb-tab>
     </sbb-tab-group>
   `,
   imports: [SbbTabsModule],
 })
-class TestComponent {}
+class TestComponent {
+  replaceLazyContent = signal(false);
+  showContent = signal(true);
+  content = signal('Lazy');
+}
--- src/angular/tabs/tab/tab.ts
+++ src/angular/tabs/tab/tab.ts
@@ -1,22 +1,24 @@
 import { CdkPortalOutlet, TemplatePortal } from '@angular/cdk/portal';
+import type { InjectionToken, OutputRef } from '@angular/core';
 import {
+  ChangeDetectorRef,
   Component,
-  ContentChild,
+  contentChild,
   ElementRef,
   inject,
-  type OutputRef,
   TemplateRef,
-  ViewChild,
   ViewContainerRef,
 } from '@angular/core';
-import { outputFromObservable } from '@angular/core/rxjs-interop';
+import { outputFromObservable, takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 import { internalOutputFromObservable } from '@sbb-esta/lyne-angular/core';
 import type { SbbTabLabelElement } from '@sbb-esta/lyne-elements/tabs/tab-label.js';
 import type { SbbTabElement } from '@sbb-esta/lyne-elements/tabs/tab.js';
-import { NEVER, fromEvent } from 'rxjs';
+import { distinctUntilChanged, fromEvent, NEVER, switchMap } from 'rxjs';
+
+import type { SbbTabContent } from './tab-content';
+import { SBB_TAB_CONTENT } from './tab-content';
 
 import '@sbb-esta/lyne-elements/tabs/tab.js';
-import { SBB_TAB_CONTENT } from './tab-content';
 
 @Component({
   selector: 'sbb-tab',
@@ -24,23 +26,23 @@ import { SBB_TAB_CONTENT } from './tab-content';
   imports: [CdkPortalOutlet],
   template: `
     <ng-template [cdkPortalOutlet]="contentPortal"></ng-template>
-    <ng-template #implicitContent>
-      <ng-content></ng-content>
-    </ng-template>
+    <ng-content></ng-content>
   `,
 })
 export class SbbTab {
   #element: ElementRef<SbbTabElement> = inject(ElementRef<SbbTabElement>);
   #viewContainerRef = inject(ViewContainerRef);
+  #changeDetectorRef = inject(ChangeDetectorRef);
+
+  // TODO(breaking-change): make protected instead of public
   public contentPortal: TemplatePortal | null = null;
 
-  @ContentChild(SBB_TAB_CONTENT, { read: TemplateRef, static: true })
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private _explicitContent?: TemplateRef<any>;
-
-  @ViewChild('implicitContent', { read: TemplateRef, static: true })
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private _implicitContent?: TemplateRef<any>;
+  private _explicitContent = contentChild<InjectionToken<SbbTabContent>, TemplateRef<unknown>>(
+    SBB_TAB_CONTENT,
+    {
+      read: TemplateRef,
+    },
+  );
 
   public get label(): SbbTabLabelElement | null {
     return this.#element.nativeElement.label;
@@ -54,11 +56,18 @@ export class SbbTab {
   );
 
   constructor() {
-    this.activeOutput.subscribe(() => {
-      this.contentPortal = new TemplatePortal(
-        (this._explicitContent || this._implicitContent)!,
-        this.#viewContainerRef,
-      );
-    });
+    const contentChildObservable = toObservable(this._explicitContent);
+    fromEvent<Event>(this.#element.nativeElement, 'active')
+      .pipe(
+        switchMap(() => contentChildObservable),
+        distinctUntilChanged(),
+        takeUntilDestroyed(),
+      )
+      .subscribe((templateRef) => {
+        this.contentPortal = templateRef
+          ? new TemplatePortal(templateRef, this.#viewContainerRef)
+          : null;
+        this.#changeDetectorRef.markForCheck();
+      });
   }
 }
