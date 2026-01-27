e5c156248701e23f3d8a8230bbbad4e38f79c49b
Jeri Peier <jeremias.peier@sbb.ch>
2026-01-27 13:56:39 +0100

    feat(sbb-expansion-panel): add lazy loading support (#276)
    Closes #272

--- src/angular/expansion-panel/expansion-panel.module.ts
+++ src/angular/expansion-panel/expansion-panel.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { SbbExpansionPanel } from './expansion-panel/expansion-panel';
 import { SbbExpansionPanelContent } from './expansion-panel-content/expansion-panel-content';
+import { SbbExpansionPanelContentDirective } from './expansion-panel-content/expansion-panel-content-directive';
 import { SbbExpansionPanelHeader } from './expansion-panel-header/expansion-panel-header';
 
 const SBB_EXPANSION_PANEL_EXPORTED_DECLARATIONS = [
   SbbExpansionPanel,
   SbbExpansionPanelContent,
+  SbbExpansionPanelContentDirective,
   SbbExpansionPanelHeader,
 ];
 
--- src/angular/expansion-panel/index.ts
+++ src/angular/expansion-panel/index.ts
@@ -1,4 +1,5 @@
 export * from './expansion-panel/expansion-panel';
 export * from './expansion-panel-content/expansion-panel-content';
+export * from './expansion-panel-content/expansion-panel-content-directive';
 export * from './expansion-panel-header/expansion-panel-header';
 export * from './expansion-panel.module';
