32a13466f511fdecadcd7740c6bef757dc747ea8
Daniel Schreiber <daniel@schreiber-lang.de>
2026-03-02 16:41:58 +0100

    fix(dialog,overlay): explicit typing for dialog/overlay return types (#292)

--- src/angular/dialog/dialog/dialog-ref.ts
+++ src/angular/dialog/dialog/dialog-ref.ts
@@ -1,4 +1,9 @@
 import { SbbOverlayBaseRef } from '@sbb-esta/lyne-angular/core/overlay';
 import type { SbbDialogCloseEvent } from '@sbb-esta/lyne-elements/dialog/dialog.js';
 
-export class SbbDialogRef<T = unknown> extends SbbOverlayBaseRef<T, SbbDialogCloseEvent> {}
+// TODO: The default type for R should be `unknown`, but it is set to `any` for backward compatibility reasons. It can be changed to `unknown` in the next major release.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export class SbbDialogRef<T = unknown, R = any> extends SbbOverlayBaseRef<
+  T,
+  SbbDialogCloseEvent<R>
+> {}
