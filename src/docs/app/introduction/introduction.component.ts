172672860131ca0c279163281b6ad4a0cb920caa
Lukas Spirig <lukas.spirig@sbb.ch>
2026-03-17 17:47:38 +0100

    chore: use sbb-link in introduction

--- src/docs/app/introduction/introduction.component.ts
+++ src/docs/app/introduction/introduction.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SbbContainer } from '@sbb-esta/lyne-angular/container/container';
+import { SbbLink } from '@sbb-esta/lyne-angular/link/link';
 import { SbbTitle } from '@sbb-esta/lyne-angular/title';
 
 @Component({
   selector: 'sbb-introduction',
   templateUrl: './introduction.component.html',
-  imports: [SbbTitle, SbbContainer],
+  imports: [SbbTitle, SbbContainer, SbbLink],
 })
 export class IntroductionComponent {}
