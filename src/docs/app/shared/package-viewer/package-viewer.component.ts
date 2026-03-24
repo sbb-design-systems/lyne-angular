c3d42ad12d1a3a2a81b533f3fb5e8d16f7fbff65
jeripeierSBB <jeremias.peier@sbb.ch>
2026-03-24 09:22:17 +0100

    feat: navigation search

--- src/docs/app/shared/package-viewer/package-viewer.component.ts
+++ src/docs/app/shared/package-viewer/package-viewer.component.ts
@@ -1,12 +1,16 @@
 import type { Signal } from '@angular/core';
-import { ChangeDetectionStrategy, Component, inject, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, viewChild } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { SbbAccordionModule } from '@sbb-esta/lyne-angular/accordion';
+import { SbbFormFieldModule } from '@sbb-esta/lyne-angular/form-field';
 import { SbbHeaderScrollOrigin } from '@sbb-esta/lyne-angular/header';
+import { SbbIcon } from '@sbb-esta/lyne-angular/icon';
 import { SbbBlockLink } from '@sbb-esta/lyne-angular/link/block-link';
 import { SbbSidebar, SbbSidebarModule } from '@sbb-esta/lyne-angular/sidebar';
-import { finalize, map } from 'rxjs/operators';
+import { SbbTitle } from '@sbb-esta/lyne-angular/title';
+import { finalize, map, startWith } from 'rxjs/operators';
 
 import type { ShowcaseMetaPackage } from '../meta';
 
@@ -17,13 +21,17 @@ import { SidebarToggle } from './sidebar-toggle';
   templateUrl: './package-viewer.component.html',
   styleUrl: './package-viewer.component.scss',
   imports: [
+    ReactiveFormsModule,
     RouterLink,
     RouterLinkActive,
     RouterOutlet,
     SbbAccordionModule,
     SbbBlockLink,
+    SbbFormFieldModule,
     SbbHeaderScrollOrigin,
+    SbbIcon,
     SbbSidebarModule,
+    SbbTitle,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -35,6 +43,26 @@ export class PackageViewerComponent {
     { initialValue: {} as ShowcaseMetaPackage },
   );
   private _sidebar = viewChild.required(SbbSidebar);
+
+  protected searchControl = new FormControl('', { nonNullable: true });
+  #searchQuery = toSignal(
+    this.searchControl.valueChanges.pipe(startWith(this.searchControl.value)),
+    { initialValue: '' },
+  );
+  protected filteredSections = computed(() => {
+    const query = this.#searchQuery();
+    const sections = this.package().sections ?? [];
+    return query
+      ? sections
+          .map((section) => ({
+            ...section,
+            entries: section.entries.filter((e) =>
+              e.label.toLowerCase().includes(query.toLowerCase()),
+            ),
+          }))
+          .filter((section) => section.entries.length > 0)
+      : sections;
+  });
 
   constructor() {
     toObservable(this._sidebar)
