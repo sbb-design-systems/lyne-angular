73bc3904c5ff3c7294ef616a6743a22887ce1e03
Davide Mininni <davide.mininni@finconsgroup.com>
2026-05-05 15:02:40 +0200

    docs(accordion): fix review

--- src/docs/app/angular/examples/accordion/accordion-variants/accordion-variants-example.ts
+++ src/docs/app/angular/examples/accordion/accordion-variants/accordion-variants-example.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { SbbAccordionModule } from '@sbb-esta/lyne-angular/accordion';
 import { SbbCheckboxModule } from '@sbb-esta/lyne-angular/checkbox';
 import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
+import { map } from 'rxjs/operators';
 
 /**
  * @title sbb-accordion with properties
@@ -15,22 +16,20 @@ import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccordionVariantsExample {
-  readonly #fb = inject(FormBuilder);
-
-  protected form = this.#fb.nonNullable.group({
+  protected form = inject(FormBuilder).nonNullable.group({
     multi: false,
     smallSize: false,
     iconPanel: false,
     disablePanel: false,
   });
 
-  readonly #smallSize = toSignal(this.form.controls.smallSize.valueChanges, {
-    initialValue: false,
-  });
-  protected readonly size = computed((): 's' | 'l' => (this.#smallSize() ? 's' : 'l'));
   protected readonly multi = toSignal(this.form.controls.multi.valueChanges, {
     initialValue: false,
   });
+  protected readonly size = toSignal(
+    this.form.controls.smallSize.valueChanges.pipe(map((size): 's' | 'l' => (size ? 's' : 'l'))),
+    { initialValue: 'l' },
+  );
   protected readonly iconPanel = toSignal(this.form.controls.iconPanel.valueChanges, {
     initialValue: false,
   });
