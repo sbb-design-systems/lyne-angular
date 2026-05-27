d4032627f58bed60cfa7521c7a8d4cdaf0bd3682
jeripeierSBB <jeremias.peier@sbb.ch>
2026-05-27 16:27:24 +0200

    fix(dialog,overlay): return null as close result for empty close attribute

--- src/angular/core/attribute-transform.ts
+++ src/angular/core/attribute-transform.ts
@@ -8,3 +8,11 @@ export function booleanAttribute(value: unknown): boolean {
     ? value
     : value !== 'false' && value !== undefined && value != null;
 }
+
+/**
+ * In case where we want to support empty inputs, we transform
+ * an empty string value to null.
+ */
+export function nullOnEmptyAttribute(value: unknown): unknown {
+  return value === '' ? null : value;
+}
