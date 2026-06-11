32e0e82179e1dfe1b945e915f3b3eaaaa78e23ec
jeripeierSBB <jeremias.peier@sbb.ch>
2026-06-11 17:29:28 +0200

    docs: enhance landing page

--- src/docs/app/introduction/introduction.component.ts
+++ src/docs/app/introduction/introduction.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SbbActionGroup } from '@sbb-esta/lyne-angular/action-group';
 import { SbbLinkModule } from '@sbb-esta/lyne-angular/link';
+import { SbbTeaser } from '@sbb-esta/lyne-angular/teaser';
 import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 
 @Component({
   selector: 'sbb-introduction',
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.scss',
-  imports: [SbbTitleModule, SbbLinkModule],
+  imports: [SbbTitleModule, SbbLinkModule, SbbTeaser, RouterLink, SbbActionGroup],
 })
 export class IntroductionComponent {}
