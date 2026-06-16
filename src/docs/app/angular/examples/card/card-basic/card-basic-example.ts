5e68957f638aa044b002b68a4cb2cfc17c44bc8c
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-06-16 11:01:48 +0200

    docs(card): add examples

--- src/docs/app/angular/examples/card/card-basic/card-basic-example.ts
+++ src/docs/app/angular/examples/card/card-basic/card-basic-example.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { SbbCardModule } from '@sbb-esta/lyne-angular/card';
+import { Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+import { type SbbCard, type SbbCardBadge, SbbCardModule } from '@sbb-esta/lyne-angular/card';
+import { SbbRadioButtonModule } from '@sbb-esta/lyne-angular/radio-button';
+import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
+
+type CardSpacing = '3x-xxs' | 'xxxs-xxs' | 'xxxs-s' | '4x-xxs' | 'xxs' | 's' | 'l' | null;
 
 /**
  * @title Basic card
+ * @order 1
  */
 @Component({
   selector: 'sbb-card-basic-example',
   templateUrl: 'card-basic-example.html',
-  imports: [SbbCardModule],
+  imports: [FormField, SbbCardModule, SbbRadioButtonModule, SbbTitleModule],
 })
-export class CardBasicExample {}
+export class CardBasicExample {
+  protected controls = form(
+    signal({
+      color: 'milk' as SbbCard['color'],
+      spacing: null as CardSpacing,
+      badgeColor: 'charcoal' as SbbCardBadge['color'],
+    }),
+  );
+}
