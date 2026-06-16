5ea5f5c655c86b0a4c7d26712c087d7b6db92abc
Lukas Spirig <lukas.spirig@sbb.ch>
2026-06-16 12:23:13 +0200

    fix(tooltip): always load tooltip element

--- src/angular/stepper/step/step-content.ts
+++ src/angular/stepper/step/step-content.ts
@@ -1,10 +1,10 @@
-import { Directive, inject, InjectionToken, TemplateRef } from '@angular/core';
+import { Directive, forwardRef, inject, InjectionToken, TemplateRef } from '@angular/core';
 
 export const SBB_STEP_CONTENT = new InjectionToken<SbbStepContent>('SbbStepContent');
 
 @Directive({
   selector: '[sbbStepContent]',
-  providers: [{ provide: SBB_STEP_CONTENT, useExisting: SbbStepContent }],
+  providers: [{ provide: SBB_STEP_CONTENT, useExisting: forwardRef(() => SbbStepContent) }],
 })
 export class SbbStepContent {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
