25d738238df469dac5511ab2aa4d96d81d03916d
Davide Mininni <davide.mininni@finconsgroup.com>
2026-06-16 11:43:23 +0200

    fix: register namespaces

--- src/docs/main.ts
+++ src/docs/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { mergeConfig } from '@sbb-esta/lyne-elements/core.js';
 
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
+
+mergeConfig({
+  icon: {
+    namespaces: new Map<string, string>()
+      .set('kom', 'https://icons.app.sbb.ch/icons/')
+      .set('fpl', 'https://icons.app.sbb.ch/icons/'),
+  },
+});
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
