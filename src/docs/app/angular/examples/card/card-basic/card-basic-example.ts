e72b3e362cb53dab56bf1ee5b0e48e70fb2057fc
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-06-16 11:41:08 +0200

    docs(card): pr feedbacks

--- src/docs/app/angular/examples/card/card-basic/card-basic-example.ts
+++ src/docs/app/angular/examples/card/card-basic/card-basic-example.ts
@@ -4,8 +4,6 @@ import { type SbbCard, type SbbCardBadge, SbbCardModule } from '@sbb-esta/lyne-a
 import { SbbRadioButtonModule } from '@sbb-esta/lyne-angular/radio-button';
 import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 
-type CardSpacing = '3x-xxs' | 'xxxs-xxs' | 'xxxs-s' | '4x-xxs' | 'xxs' | 's' | 'l' | null;
-
 /**
  * @title Basic card
  * @order 1
@@ -19,7 +17,7 @@ export class CardBasicExample {
   protected controls = form(
     signal({
       color: 'milk' as SbbCard['color'],
-      spacing: null as CardSpacing,
+      spacing: null as string | null,
       badgeColor: 'charcoal' as SbbCardBadge['color'],
     }),
   );
