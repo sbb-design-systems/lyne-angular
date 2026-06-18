0b5b735dc8b38e974b99888a09498225f1c447b3
jeripeierSBB <jeremias.peier@sbb.ch>
2026-06-18 10:21:23 +0200

    fix: jeris ideas

--- src/docs/app/introduction/introduction.component.ts
+++ src/docs/app/introduction/introduction.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { SbbActionGroup } from '@sbb-esta/lyne-angular/action-group';
+import { SbbActionGroupModule } from '@sbb-esta/lyne-angular/action-group';
+import { SbbButtonModule } from '@sbb-esta/lyne-angular/button';
+import { SbbCardModule } from '@sbb-esta/lyne-angular/card';
+import { SbbChipLabelModule } from '@sbb-esta/lyne-angular/chip-label';
 import { SbbLinkModule } from '@sbb-esta/lyne-angular/link';
-import { SbbTeaser } from '@sbb-esta/lyne-angular/teaser';
+import { SbbLinkListModule } from '@sbb-esta/lyne-angular/link-list';
+import { SbbTeaserModule } from '@sbb-esta/lyne-angular/teaser';
 import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 
 @Component({
   selector: 'sbb-introduction',
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.scss',
-  imports: [SbbTitleModule, SbbLinkModule, SbbTeaser, RouterLink, SbbActionGroup],
+  imports: [
+    RouterLink,
+    SbbActionGroupModule,
+    SbbButtonModule,
+    SbbCardModule,
+    SbbChipLabelModule,
+    SbbLinkListModule,
+    SbbLinkModule,
+    SbbTeaserModule,
+    SbbTitleModule,
+  ],
 })
 export class IntroductionComponent {}
