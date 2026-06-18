515878154c194a8b59afecb80ef03588ec217142
Davide Mininni <davide.mininni@finconsgroup.com>
2026-06-18 17:41:49 +0200

    fix: review Jeri

--- src/docs/app/angular/icon-overview/cdn-icon-list/cdn-icon-dialog/cdn-icon-dialog.component.ts
+++ src/docs/app/angular/icon-overview/cdn-icon-list/cdn-icon-dialog/cdn-icon-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { SbbButtonModule } from '@sbb-esta/lyne-angular/button';
 import { SBB_OVERLAY_DATA } from '@sbb-esta/lyne-angular/core';
 import { SbbDialogModule } from '@sbb-esta/lyne-angular/dialog';
@@ -16,17 +16,17 @@ import type { CdnIcon } from '../../cdn-icon.service';
 export class CdnIconDialogComponent {
   data = inject<{ cdnIcon: CdnIcon }>(SBB_OVERLAY_DATA);
 
-  get cdnIcon(): CdnIcon {
+  cdnIcon = computed(() => {
     return this.data.cdnIcon;
-  }
+  });
 
-  get tags(): string {
-    return this.cdnIcon.tags.join(', ');
-  }
+  tags = computed(() => {
+    return this.cdnIcon().tags.join(', ');
+  });
 
-  get cdnIconPath() {
-    return this.cdnIcon.namespace
-      ? `${this.cdnIcon.namespace}:${this.cdnIcon.name}`
-      : this.cdnIcon.name;
-  }
+  cdnIconPath = computed(() => {
+    return this.cdnIcon().namespace
+      ? `${this.cdnIcon().namespace}:${this.cdnIcon().name}`
+      : this.cdnIcon().name;
+  });
 }
