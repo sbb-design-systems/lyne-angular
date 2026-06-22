5b91f73fef9623334976021cde103bcb3ca12246
Mario Castigliano <mario.castigliano@finconsgroup.com>
2026-06-22 10:47:12 +0200

    docs(checkbox): implement examples

---
+++ src/docs/app/angular/examples/checkbox/checkbox-long-label/checkbox-long-label-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SbbCheckboxModule } from '@sbb-esta/lyne-angular/checkbox';
+
+/**
+ * @title Checkbox with a long label
+ */
+@Component({
+  selector: 'sbb-checkbox-long-label-example',
+  templateUrl: 'checkbox-long-label-example.html',
+  imports: [SbbCheckboxModule],
+})
+export class CheckboxLongLabelExample {}
