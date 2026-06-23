67d6c2cb6a050dfa61d62e7b557c1128bf3c02ba
Davide Mininni <davide.mininni@finconsgroup.com>
2026-06-23 12:04:19 +0200

    docs: improve stories

--- src/docs/app/angular/examples/date-input/date-input-basic/date-input-basic-example.ts
+++ src/docs/app/angular/examples/date-input/date-input-basic/date-input-basic-example.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { SbbDateInputModule } from '@sbb-esta/lyne-angular/date-input';
+import { Component, computed, signal, viewChild } from '@angular/core';
+import { SbbDateInput, SbbDateInputModule } from '@sbb-esta/lyne-angular/date-input';
 import { SbbFormFieldModule } from '@sbb-esta/lyne-angular/form-field';
+import { defaultDateAdapter } from '@sbb-esta/lyne-elements/core.js';
 
 /**
  * @title Basic date-input
@@ -10,4 +11,18 @@ import { SbbFormFieldModule } from '@sbb-esta/lyne-angular/form-field';
   templateUrl: 'date-input-basic-example.html',
   imports: [SbbDateInputModule, SbbFormFieldModule],
 })
-export class DateInputBasicExample {}
+export class DateInputBasicExample {
+  protected readonly dateInput = viewChild.required(SbbDateInput);
+  protected readonly dateValue = signal<string>('2024-12-12');
+  protected readonly valueAsDate = computed(() => this.#internalCurrentDate());
+  protected readonly valueISO8601 = computed(() => {
+    const date = this.#internalCurrentDate();
+    return date ? defaultDateAdapter.toIso8601(date) : 'null';
+  });
+
+  readonly #internalCurrentDate = signal<Date | null>(new Date('2024-12-12'));
+
+  onInput(): void {
+    this.#internalCurrentDate.set(this.dateInput().valueAsDate);
+  }
+}
