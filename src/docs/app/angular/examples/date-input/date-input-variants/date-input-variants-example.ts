13426c39a6d4286a546a230e70f3ffeab8c9cf77
Davide Mininni <davide.mininni@finconsgroup.com>
2026-06-29 11:14:51 +0200

    fix: review Jeri

--- src/docs/app/angular/examples/date-input/date-input-variants/date-input-variants-example.ts
+++ src/docs/app/angular/examples/date-input/date-input-variants/date-input-variants-example.ts
@@ -5,7 +5,7 @@ import { SbbDateInputModule } from '@sbb-esta/lyne-angular/date-input';
 import { SbbFormFieldModule } from '@sbb-esta/lyne-angular/form-field';
 import { SbbRadioButtonModule } from '@sbb-esta/lyne-angular/radio-button';
 import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
-import type { SbbRadioButtonElement } from '@sbb-esta/lyne-elements/radio-button.pure.js';
+import type { SbbDateInputElement } from '@sbb-esta/lyne-elements/date-input.pure.js';
 
 /**
  * @title date-input with configurable properties
@@ -25,6 +25,10 @@ import type { SbbRadioButtonElement } from '@sbb-esta/lyne-elements/radio-button
   ],
 })
 export class DateInputVariantsExample {
+  protected noFilter: SbbDateInputElement['dateFilter'] = () => true;
+  protected filterOdd: SbbDateInputElement['dateFilter'] = (d: Date) => d.getFullYear() % 2 === 0;
+  protected filterEven: SbbDateInputElement['dateFilter'] = (d: Date) => d.getFullYear() % 2 === 1;
+
   protected form = new FormBuilder().group({
     dateValue: new FormControl(new Date('2024-12-12')),
     min: new FormControl(new Date('2024-01-01')),
@@ -32,14 +36,4 @@ export class DateInputVariantsExample {
     disabled: new FormControl(false),
     dateFilter: new FormControl<(d: Date) => boolean>(() => true),
   });
-
-  onChange(event: Event): void {
-    const radio = event.target as SbbRadioButtonElement;
-    if (!radio.checked) {
-      return this.form.controls.dateFilter.patchValue(() => true);
-    }
-    this.form.controls.dateFilter.patchValue(
-      (d: Date) => d.getFullYear() % 2 === (radio.value === 'odd' ? 0 : 1),
-    );
-  }
 }
