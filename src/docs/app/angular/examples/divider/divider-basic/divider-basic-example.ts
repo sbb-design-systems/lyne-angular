291be7fe78c4169d764dd3b76fc1d18a1de27498
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-06-29 10:05:45 +0200

    docs(divider): pr feedbacks

--- src/docs/app/angular/examples/divider/divider-basic/divider-basic-example.ts
+++ src/docs/app/angular/examples/divider/divider-basic/divider-basic-example.ts
@@ -1,8 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { form, FormField } from '@angular/forms/signals';
-import { SbbCheckbox } from '@sbb-esta/lyne-angular/checkbox';
+import { SbbCheckboxModule } from '@sbb-esta/lyne-angular/checkbox';
 import { SbbDividerModule } from '@sbb-esta/lyne-angular/divider';
-import { SbbTitle } from '@sbb-esta/lyne-angular/title';
+import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 
 /**
  * @title Basic divider
@@ -12,7 +12,7 @@ import { SbbTitle } from '@sbb-esta/lyne-angular/title';
   selector: 'sbb-divider-basic-example',
   templateUrl: 'divider-basic-example.html',
   styleUrl: 'divider-basic-example.scss',
-  imports: [FormField, SbbCheckbox, SbbDividerModule, SbbTitle],
+  imports: [FormField, SbbCheckboxModule, SbbDividerModule, SbbTitleModule],
 })
 export class DividerBasicExample {
   protected controls = form(
