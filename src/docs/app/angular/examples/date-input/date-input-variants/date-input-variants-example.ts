8cab6074f95ccf236789a5a5290279492eeb1275
Davide Mininni <davide.mininni@finconsgroup.com>
2026-06-30 11:57:22 +0200

    fix: change controls to validators

--- src/docs/app/angular/examples/date-input/date-input-variants/date-input-variants-example.ts
+++ src/docs/app/angular/examples/date-input/date-input-variants/date-input-variants-example.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import type { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { SbbCheckboxModule } from '@sbb-esta/lyne-angular/checkbox';
 import { SbbDateInputModule } from '@sbb-esta/lyne-angular/date-input';
@@ -6,6 +8,38 @@ import { SbbFormFieldModule } from '@sbb-esta/lyne-angular/form-field';
 import { SbbRadioButtonModule } from '@sbb-esta/lyne-angular/radio-button';
 import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 import type { SbbDateInputElement } from '@sbb-esta/lyne-elements/date-input.pure.js';
+
+function dateConstraintsValidator(): ValidatorFn {
+  return (group: AbstractControl): ValidationErrors | null => {
+    const dateValue: AbstractControl<Date | null> = group.get('dateValue')!;
+    const disabled: boolean = group.get('disabled')!.value;
+
+    if (disabled && dateValue.enabled) {
+      dateValue.disable({ emitEvent: false });
+    } else if (!disabled && dateValue.disabled) {
+      dateValue.enable({ emitEvent: false });
+    }
+
+    const errors: ValidationErrors = {};
+    const value: Date | null = dateValue.value;
+    const min: Date | null = group.get('min')!.value ?? null;
+    const max: Date | null = group.get('max')!.value ?? null;
+    const filterFn: ((d: Date) => boolean) | null = group.get('dateFilter')!.value;
+
+    if (value && min && value < min) {
+      errors['min'] = { min, actual: value };
+    }
+    if (value && max && value > max) {
+      errors['max'] = { max, actual: value };
+    }
+    if (value && filterFn && !filterFn(value)) {
+      errors['dateFilter'] = { actual: value };
+    }
+
+    dateValue.setErrors(Object.keys(errors).length ? errors : null, { emitEvent: false });
+    return null;
+  };
+}
 
 /**
  * @title date-input with configurable properties
@@ -15,13 +49,13 @@ import type { SbbDateInputElement } from '@sbb-esta/lyne-elements/date-input.pur
   selector: 'sbb-date-input-variants-example',
   templateUrl: 'date-input-variants-example.html',
   imports: [
+    ReactiveFormsModule,
+    DatePipe,
     SbbCheckboxModule,
+    SbbDateInputModule,
     SbbFormFieldModule,
     SbbRadioButtonModule,
     SbbTitleModule,
-    SbbDateInputModule,
-    SbbFormFieldModule,
-    ReactiveFormsModule,
   ],
 })
 export class DateInputVariantsExample {
@@ -33,11 +67,14 @@ export class DateInputVariantsExample {
    * TODO: replace with signal forms and move min/max/disabled/dateFilter to validators
    *  when PR https://github.com/angular/angular/pull/69452 is merged.
    */
-  protected form = new FormBuilder().group({
-    dateValue: new FormControl(new Date('2024-12-12')),
-    min: new FormControl(new Date('2024-01-01')),
-    max: new FormControl(new Date('2027-01-01')),
-    disabled: new FormControl(false),
-    dateFilter: new FormControl<(d: Date) => boolean>(() => true),
-  });
+  protected form = new FormBuilder().group(
+    {
+      dateValue: new FormControl<Date | null>(new Date('2024-12-12')),
+      min: new FormControl<Date | null>(new Date('2024-01-01')),
+      max: new FormControl<Date | null>(new Date('2027-01-01')),
+      disabled: new FormControl<boolean>(false, { nonNullable: true }),
+      dateFilter: new FormControl<(d: Date) => boolean>(() => true, { nonNullable: true }),
+    },
+    { validators: dateConstraintsValidator() },
+  );
 }
