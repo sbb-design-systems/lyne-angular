a67fe6dac282c42c9843fcbe30e0c1080b6de9d3
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-07-01 14:53:57 +0200

    docs(header): add examples

--- src/docs/app/angular/examples/header/header-basic/header-basic-example.ts
+++ src/docs/app/angular/examples/header/header-basic/header-basic-example.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
-import { SbbHeaderModule } from '@sbb-esta/lyne-angular/header';
+import { Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+import { SbbCheckboxModule } from '@sbb-esta/lyne-angular/checkbox';
+import { SbbContainerModule } from '@sbb-esta/lyne-angular/container';
+import { type SbbHeader, SbbHeaderModule } from '@sbb-esta/lyne-angular/header';
 import { SbbLogoModule } from '@sbb-esta/lyne-angular/logo';
+import { SbbRadioButtonModule } from '@sbb-esta/lyne-angular/radio-button';
+import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 
 /**
  * @title Basic header
@@ -8,6 +13,22 @@ import { SbbLogoModule } from '@sbb-esta/lyne-angular/logo';
 @Component({
   selector: 'sbb-header-basic-example',
   templateUrl: 'header-basic-example.html',
-  imports: [SbbHeaderModule, SbbLogoModule],
+  imports: [
+    FormField,
+    SbbCheckboxModule,
+    SbbContainerModule,
+    SbbHeaderModule,
+    SbbLogoModule,
+    SbbRadioButtonModule,
+    SbbTitleModule,
+  ],
 })
-export class HeaderBasicExample {}
+export class HeaderBasicExample {
+  protected controls = form(
+    signal({
+      expanded: false,
+      hideOnScroll: false,
+      size: null as SbbHeader['size'],
+    }),
+  );
+}
