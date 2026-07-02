c182d7cf3145a37395445cec2e8b57615a1fe0ef
Davide Mininni <davide.mininni@finconsgroup.com>
2026-07-02 11:08:01 +0200

    fix: review

--- src/docs/app/angular/examples/chip/chip-variants/chip-variants-example.ts
+++ src/docs/app/angular/examples/chip/chip-variants/chip-variants-example.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { disabled, form, FormField } from '@angular/forms/signals';
+import { form, FormField } from '@angular/forms/signals';
 import { SbbCheckboxModule } from '@sbb-esta/lyne-angular/checkbox';
 import { SbbChipModule } from '@sbb-esta/lyne-angular/chip';
 import type { SbbFormField } from '@sbb-esta/lyne-angular/form-field';
@@ -32,13 +32,5 @@ export class ChipVariantsExample {
       size: null as SbbFormField['size'],
       hiddenLabel: false,
     }),
-    (schemaPath) => {
-      disabled(schemaPath.disabled, {
-        when: ({ valueOf }) => valueOf(schemaPath.readonly),
-      });
-      disabled(schemaPath.readonly, {
-        when: ({ valueOf }) => valueOf(schemaPath.disabled),
-      });
-    },
   );
 }
