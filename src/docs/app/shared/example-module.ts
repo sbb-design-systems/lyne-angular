6cfa63a94a3b78e17570e7b1c10f765bdb3d6e0b
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-07-06 10:32:52 +0200

    docs(journey-header): add examples

--- src/docs/app/shared/example-module.ts
+++ src/docs/app/shared/example-module.ts
@@ -79,7 +79,7 @@ export const EXAMPLE_COMPONENTS: Record<string, (string | Partial<ExampleData>)[
   icon: ['icon-basic'],
   'icon-sidebar': [{ id: 'icon-sidebar-basic', hasStyle: true }],
   image: ['image-basic'],
-  'journey-header': ['journey-header-basic'],
+  'journey-header': ['journey-header-showcase'],
   'lead-container': ['lead-container-basic'],
   link: ['link-basic'],
   'link-list': ['link-list-basic'],
@@ -263,7 +263,7 @@ export async function loadExample(id: string): Promise<Record<string, Type<unkno
       return import('../angular/examples/icon-sidebar');
     case 'image-basic':
       return import('../angular/examples/image');
-    case 'journey-header-basic':
+    case 'journey-header-showcase':
       return import('../angular/examples/journey-header');
     case 'lead-container-basic':
       return import('../angular/examples/lead-container');
