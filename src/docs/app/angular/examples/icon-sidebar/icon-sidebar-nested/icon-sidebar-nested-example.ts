3d9bc1fd8483676c7cf5cd7b880ea7ce6769e269
Davide Mininni <davide.mininni@finconsgroup.com>
2026-07-07 12:34:16 +0200

    docs: fix review

--- src/docs/app/angular/examples/icon-sidebar/icon-sidebar-nested/icon-sidebar-nested-example.ts
+++ src/docs/app/angular/examples/icon-sidebar/icon-sidebar-nested/icon-sidebar-nested-example.ts
@@ -1,11 +1,16 @@
-import { Component, viewChild } from '@angular/core';
+import { Component, DestroyRef, inject, signal, viewChild } from '@angular/core';
+import { outputToObservable, takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 import { SbbButtonModule } from '@sbb-esta/lyne-angular/button';
+import { SbbCardModule } from '@sbb-esta/lyne-angular/card';
+import { SbbDividerModule } from '@sbb-esta/lyne-angular/divider';
+import { SbbHeaderModule } from '@sbb-esta/lyne-angular/header';
 import { SbbIconSidebarModule } from '@sbb-esta/lyne-angular/icon-sidebar';
 import { SbbLinkModule } from '@sbb-esta/lyne-angular/link';
 import { SbbLinkListModule } from '@sbb-esta/lyne-angular/link-list';
 import { SbbSidebar, SbbSidebarModule } from '@sbb-esta/lyne-angular/sidebar';
 import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 import { SbbTooltipModule } from '@sbb-esta/lyne-angular/tooltip';
+import { merge } from 'rxjs';
 
 /**
  * @title icon-sidebar nested with sidebar
@@ -16,19 +21,31 @@ import { SbbTooltipModule } from '@sbb-esta/lyne-angular/tooltip';
   templateUrl: 'icon-sidebar-nested-example.html',
   styleUrl: 'icon-sidebar-nested-example.scss',
   imports: [
-    SbbIconSidebarModule,
+    SbbCardModule,
     SbbButtonModule,
-    SbbTooltipModule,
-    SbbSidebarModule,
-    SbbLinkListModule,
+    SbbIconSidebarModule,
+    SbbHeaderModule,
     SbbLinkModule,
+    SbbLinkListModule,
+    SbbSidebarModule,
     SbbTitleModule,
+    SbbTooltipModule,
+    SbbDividerModule,
   ],
 })
 export class IconSidebarNestedExample {
-  private _sidebar = viewChild.required(SbbSidebar);
+  protected opened = signal<boolean>(true);
+  protected sidebar = viewChild.required(SbbSidebar);
+  private destroyRef = inject(DestroyRef);
 
-  protected toggleSidebar() {
-    this._sidebar().toggle();
+  constructor() {
+    toObservable(this.sidebar).subscribe((sidebar) => {
+      merge(
+        outputToObservable(sidebar.beforeOpenOutput),
+        outputToObservable(sidebar.beforeCloseOutput),
+      )
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe((event) => this.opened.set(event?.type === 'beforeopen'));
+    });
   }
 }
