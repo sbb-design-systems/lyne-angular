e051a4d130fd415e4d267527f1528798812219e1
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-07-13 09:26:32 +0200

    docs(option): add examples

---
+++ src/docs/app/angular/examples/option/option-showcase/option-showcase-example.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+import { SbbCheckboxModule } from '@sbb-esta/lyne-angular/checkbox';
+import { SbbOptionModule } from '@sbb-esta/lyne-angular/option';
+import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
+
+/**
+ * @title sbb-option showcase
+ * @order 1
+ */
+@Component({
+  selector: 'sbb-option-showcase-example',
+  templateUrl: 'option-showcase-example.html',
+  imports: [FormField, SbbCheckboxModule, SbbOptionModule, SbbTitleModule],
+})
+export class OptionShowcaseExample {
+  protected controls = form(
+    signal({
+      selected: false,
+      disabled: false,
+    }),
+  );
+}
