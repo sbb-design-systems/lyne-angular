adc37b1eeeed718192fc752e91470811646749ee
Tommaso Menga <tommaso.menga-94@hotmail.it>
2026-07-16 10:17:56 +0200

    docs(selection-action-panel): add examples (#504)

--- src/docs/app/angular/examples/selection-action-panel/selection-action-panel-group/selection-action-panel-group-example.ts
+++ src/docs/app/angular/examples/selection-action-panel/selection-action-panel-group/selection-action-panel-group-example.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
 import { SbbButtonModule } from '@sbb-esta/lyne-angular/button';
 import { SbbCardModule } from '@sbb-esta/lyne-angular/card';
-import { SbbCheckboxPanelModule } from '@sbb-esta/lyne-angular/checkbox-panel';
 import { SbbRadioButtonPanelModule } from '@sbb-esta/lyne-angular/radio-button-panel';
 import { SbbSelectionActionPanelModule } from '@sbb-esta/lyne-angular/selection-action-panel';
 
 /**
- * @title Basic selection-action-panel
- * @order 1
+ * @title selection-action-panel in a group
+ * @order 3
  */
 @Component({
-  selector: 'sbb-selection-action-panel-basic-example',
-  templateUrl: 'selection-action-panel-basic-example.html',
+  selector: 'sbb-selection-action-panel-group-example',
+  templateUrl: 'selection-action-panel-group-example.html',
   imports: [
     SbbCardModule,
-    SbbCheckboxPanelModule,
     SbbSelectionActionPanelModule,
     SbbRadioButtonPanelModule,
     SbbButtonModule,
   ],
 })
-export class SelectionActionPanelBasicExample {}
+export class SelectionActionPanelGroupExample {}
