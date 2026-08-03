2c529dc99e2b896074f6ca717308cec022406d8a
Davide Mininni <101575400+DavideMininni-Fincons@users.noreply.github.com>
2026-08-03 09:45:19 +0200

    docs(time-input): add examples (#529)
    Subtask of #358.

--- src/docs/app/angular/examples/time-input/index.ts
+++ src/docs/app/angular/examples/time-input/index.ts
@@ -1,1 +1,4 @@
-export { TimeInputBasicExample } from './time-input-basic/time-input-basic-example';
+export { TimeInputReactiveExample } from './time-input-reactive/time-input-reactive-example';
+export { TimeInputShowcaseExample } from './time-input-showcase/time-input-showcase-example';
+export { TimeInputSignalExample } from './time-input-signal/time-input-signal-example';
+export { TimeInputTemplateDrivenExample } from './time-input-template-driven/time-input-template-driven-example';
