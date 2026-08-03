9124cc3209a079e3d07c78624036b5f719aa03c3
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-08-03 17:18:27 +0200

    fix: pr feedbacks

--- src/docs/app/angular/examples/train/train-showcase/train-showcase-example.ts
+++ src/docs/app/angular/examples/train/train-showcase/train-showcase-example.ts
@@ -3,7 +3,7 @@ import { form, FormField } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { SbbIconModule } from '@sbb-esta/lyne-angular/icon';
 import { SbbRadioButtonModule } from '@sbb-esta/lyne-angular/radio-button';
-import { SbbTitle } from '@sbb-esta/lyne-angular/title';
+import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 import { type SbbTrainFormation, SbbTrainModule } from '@sbb-esta/lyne-angular/train';
 
 /**
@@ -13,7 +13,14 @@ import { type SbbTrainFormation, SbbTrainModule } from '@sbb-esta/lyne-angular/t
 @Component({
   selector: 'sbb-train-showcase-example',
   templateUrl: 'train-showcase-example.html',
-  imports: [FormField, RouterLink, SbbTrainModule, SbbIconModule, SbbTitle, SbbRadioButtonModule],
+  imports: [
+    FormField,
+    RouterLink,
+    SbbTrainModule,
+    SbbIconModule,
+    SbbTitleModule,
+    SbbRadioButtonModule,
+  ],
 })
 export class TrainShowcaseExample {
   protected controls = form(
--- src/docs/app/angular/examples/train/train-wagon-showcase/train-wagon-showcase-example.ts
+++ src/docs/app/angular/examples/train/train-wagon-showcase/train-wagon-showcase-example.ts
@@ -1,10 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { disabled, form, FormField } from '@angular/forms/signals';
+import { SbbCheckboxModule } from '@sbb-esta/lyne-angular/checkbox';
 import { SbbFormFieldModule } from '@sbb-esta/lyne-angular/form-field';
+import { SbbIconModule } from '@sbb-esta/lyne-angular/icon';
 import { SbbRadioButtonModule } from '@sbb-esta/lyne-angular/radio-button';
 import { SbbSelectModule } from '@sbb-esta/lyne-angular/select';
 import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
-import { type SbbTrainWagon, SbbTrainModule } from '@sbb-esta/lyne-angular/train';
+import {
+  type SbbTrainWagon,
+  SbbTrainModule,
+  type SbbTrainFormation,
+} from '@sbb-esta/lyne-angular/train';
 
 /**
  * @title `sbb-train-wagon` showcase
@@ -15,7 +21,9 @@ import { type SbbTrainWagon, SbbTrainModule } from '@sbb-esta/lyne-angular/train
   templateUrl: 'train-wagon-showcase-example.html',
   imports: [
     FormField,
+    SbbCheckboxModule,
     SbbFormFieldModule,
+    SbbIconModule,
     SbbRadioButtonModule,
     SbbSelectModule,
     SbbTitleModule,
@@ -27,8 +35,21 @@ export class TrainWagonShowcaseExample {
     signal({
       wagonType: 'wagon' as SbbTrainWagon['wagonType'],
       occupancy: 'none' as SbbTrainWagon['occupancy'],
-      wagonClass: '1' as NonNullable<SbbTrainWagon['wagonClass']>,
+      wagonClass: '1' as SbbTrainWagon['wagonClass'],
+      withIcons: false,
       label: 'label',
+      view: 'side' as SbbTrainFormation['view'],
+      orientation: 'horizontal' as SbbTrainFormation['orientation'],
     }),
+    (schemaPath) => {
+      disabled(schemaPath.occupancy, {
+        when: ({ valueOf }) =>
+          ['couchette', 'sleeping', 'restaurant'].includes(valueOf(schemaPath.wagonType)),
+      });
+      disabled(schemaPath.wagonClass, {
+        when: ({ valueOf }) =>
+          ['couchette', 'sleeping', 'restaurant'].includes(valueOf(schemaPath.wagonType)),
+      });
+    },
   );
 }
