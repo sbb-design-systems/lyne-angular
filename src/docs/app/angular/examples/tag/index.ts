b49682702b1aeaafadff4b2239e1ea4632879a07
Jeri Peier <jeremias.peier@sbb.ch>
2026-08-04 16:04:18 +0200

    docs(tag): add filter example (#542)

--- src/docs/app/angular/examples/tag/index.ts
+++ src/docs/app/angular/examples/tag/index.ts
@@ -1,3 +1,3 @@
-export { TagAllExample } from './tag-all/tag-all-example';
+export { TagAllFilterExample } from './tag-all-filter/tag-all-filter-example';
 export { TagExclusiveExample } from './tag-exclusive/tag-exclusive-example';
 export { TagShowcaseExample } from './tag-showcase/tag-showcase-example';
