1b1c6a84d3651b77967460a11749b66d9a0a8467
Tommmaso Menga <tommaso.menga-94@hotmail.it>
2026-08-06 11:00:29 +0200

    docs(map-container): improve examples

--- src/docs/app/angular/examples/map-container/map-container-basic/map-container-basic-example.ts
+++ src/docs/app/angular/examples/map-container/map-container-basic/map-container-basic-example.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { SbbCardModule } from '@sbb-esta/lyne-angular/card';
+import { SbbFormFieldModule } from '@sbb-esta/lyne-angular/form-field';
+import { SbbHeaderModule } from '@sbb-esta/lyne-angular/header';
+import { SbbIconModule } from '@sbb-esta/lyne-angular/icon';
+import { SbbLogoModule } from '@sbb-esta/lyne-angular/logo';
 import { SbbMapContainerModule } from '@sbb-esta/lyne-angular/map-container';
+import { SbbTitleModule } from '@sbb-esta/lyne-angular/title';
 
 /**
  * @title Basic map-container
@@ -7,7 +13,16 @@ import { SbbMapContainerModule } from '@sbb-esta/lyne-angular/map-container';
 @Component({
   selector: 'sbb-map-container-basic-example',
   templateUrl: 'map-container-basic-example.html',
-  imports: [SbbMapContainerModule],
+  styleUrl: 'map-container-basic-example.scss',
+  imports: [
+    SbbCardModule,
+    SbbFormFieldModule,
+    SbbHeaderModule,
+    SbbIconModule,
+    SbbLogoModule,
+    SbbMapContainerModule,
+    SbbTitleModule,
+  ],
   host: { class: 'sbb-example-fullscreen-only' },
 })
 export class MapContainerBasicExample {}
