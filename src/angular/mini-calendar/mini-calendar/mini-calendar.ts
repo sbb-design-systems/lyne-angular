a202ed8cf93037d5ce0ce1cb22d62861bb9b571a
Lukas Spirig <lukas.spirig@sbb.ch>
2026-08-31 17:04:13 +0200

    feat(mini-calendar): implement `SbbMiniCalendarDataSource` usage (#578)
    This PR introduces `SbbMiniCalendarDataSource`. This allows an easier
    configuration of the mini calendar.

--- src/angular/mini-calendar/mini-calendar/mini-calendar.ts
+++ src/angular/mini-calendar/mini-calendar/mini-calendar.ts
@@ -1,14 +1,59 @@
-import { Directive, ElementRef, Input, NgZone, inject } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  Input,
+  NgZone,
+  TemplateRef,
+  contentChild,
+  inject,
+  signal,
+} from '@angular/core';
 import { SbbMiniCalendarElement } from '@sbb-esta/lyne-elements/mini-calendar.pure.js';
+
+import { SbbMiniCalendarDay } from '../mini-calendar-day/mini-calendar-day';
+import { SbbMiniCalendarMonth } from '../mini-calendar-month/mini-calendar-month';
+
+import type { SbbMiniCalendarDataSource } from './mini-calendar-data-source';
+import {
+  SBB_MINI_CALENDAR_DAY_TEMPLATE,
+  type SbbMiniCalendarDayTemplate,
+} from './mini-calendar-day-template';
 
 /**
  * It displays a minimal calendar, together with the `sbb-mini-calendar-month` and `sbb-mini-calendar-day`.
  *
  * @slot  - Use the unnamed slot to add `sbb-mini-calendar-month` elements.
  */
-@Directive({
+@Component({
   selector: 'sbb-mini-calendar',
   exportAs: 'sbbMiniCalendar',
+  imports: [SbbMiniCalendarMonth, SbbMiniCalendarDay, NgTemplateOutlet],
+  template: `
+    @if (_dataSource(); as data) {
+      @for (month of data.months; track month.value) {
+        <sbb-mini-calendar-month [date]="month.value">
+          @for (day of month.days; track day().date) {
+            @if (dayTemplate(); as template) {
+              <ng-container
+                *ngTemplateOutlet="template; context: { $implicit: day }"
+              ></ng-container>
+            } @else {
+              <sbb-mini-calendar-day
+                [date]="day().date"
+                [value]="day().value ?? ''"
+                [marker]="day().marker ?? ''"
+                [color]="day().color ?? ''"
+                (click)="data['click']?.(day)"
+              ></sbb-mini-calendar-day>
+            }
+          }
+        </sbb-mini-calendar-month>
+      }
+    } @else {
+      <ng-content></ng-content>
+    }
+  `,
 })
 export class SbbMiniCalendar<T = Date> {
   static {
@@ -17,6 +62,12 @@ export class SbbMiniCalendar<T = Date> {
 
   #element: ElementRef<SbbMiniCalendarElement<T>> = inject(ElementRef<SbbMiniCalendarElement<T>>);
   #ngZone: NgZone = inject(NgZone);
+  protected dayTemplate = contentChild<SbbMiniCalendarDayTemplate, TemplateRef<unknown>>(
+    SBB_MINI_CALENDAR_DAY_TEMPLATE,
+    {
+      read: TemplateRef,
+    },
+  );
 
   /**
    * The orientation of days in the calendar.
@@ -28,4 +79,17 @@ export class SbbMiniCalendar<T = Date> {
   public get orientation(): 'horizontal' | 'vertical' {
     return this.#element.nativeElement.orientation;
   }
+
+  /**
+   * The data source of the calendar. If not provided, the calendar will display
+   * the months and days that are slotted in the component.
+   */
+  @Input()
+  public set dataSource(value: SbbMiniCalendarDataSource | null) {
+    this._dataSource.set(value);
+  }
+  public get dataSource(): SbbMiniCalendarDataSource | null {
+    return this._dataSource();
+  }
+  protected _dataSource = signal<SbbMiniCalendarDataSource | null>(null);
 }
--- src/docs/app/angular/examples/mini-calendar/index.ts
+++ src/docs/app/angular/examples/mini-calendar/index.ts
@@ -1,2 +1,4 @@
 export { MiniCalendarShowcaseExample } from './mini-calendar-showcase/mini-calendar-showcase-example';
+export { MiniCalendarWithDataSourceExample } from './mini-calendar-with-data-source/mini-calendar-with-data-source-example';
+export { MiniCalendarWithTooltipAndDataSourceExample } from './mini-calendar-with-tooltip-and-data-source/mini-calendar-with-tooltip-and-data-source-example';
 export { MiniCalendarWithTooltipExample } from './mini-calendar-with-tooltip/mini-calendar-with-tooltip-example';
